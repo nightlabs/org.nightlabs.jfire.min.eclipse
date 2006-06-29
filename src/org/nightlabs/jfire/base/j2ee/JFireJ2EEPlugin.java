df6cfee4a17cb3c8f34ebb2eba85c26370720a9e
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2006-06-29 20:58:46 +0000

    - o.n.j.b.j2ee.osgi: deployment as JAR (directory does not work) - o.n.j.b.j2ee + o.n.jfire.base: updating MANIFEST.MF with remote packages
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.j2ee@4071 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/j2ee/JFireJ2EEPlugin.java
+++ src/org/nightlabs/jfire/base/j2ee/JFireJ2EEPlugin.java
@@ -1,6 +1,25 @@
 package org.nightlabs.jfire.base.j2ee;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.nightlabs.jfire.classloader.JFireRCDLDelegate;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -47,4 +66,76 @@ public class JFireJ2EEPlugin extends AbstractUIPlugin {
 		return plugin;
 	}
 
+	/**
+	 * This method rewrites (if necessary) the MANIFEST.MF of this plugin (i.e. <code>org.nightlabs.jfire.base.j2ee</code>).
+	 *
+	 * @return <code>true</code>, if the file had to be modified (and thus a reboot of the RCP is necessary).
+	 * @throws IOException 
+	 * @throws URISyntaxException 
+	 */
+	public boolean updateManifest() throws IOException, URISyntaxException
+	{
+		System.out.println("****************************************************************");
+
+		// find the MANIFEST.MF
+		File manifestFile;
+		{
+//			Bundle bundle = Platform.getBundle(PLUGIN_ID);
+			Bundle bundle = getBundle();
+			Path path = new Path("META-INF/MANIFEST.MF");
+			URL fileURL = FileLocator.find(bundle, path, null);
+			URL realURL = FileLocator.resolve(fileURL);
+			manifestFile = new File(realURL.toURI());
+			if (!manifestFile.exists())
+				throw new IllegalStateException("The plugin's MANIFEST.MF does not exist: " + manifestFile.getAbsolutePath());
+		}
+
+		// read the MANIFEST.MF
+		InputStream in = new FileInputStream(manifestFile);
+		Manifest manifest;
+		try {
+			manifest = new Manifest(in);
+		} finally {
+			in.close();
+		}
+
+		String exportPackage = manifest.getMainAttributes().getValue("Export-Package");
+
+		// parse the comma-separated packages
+		StringTokenizer st = new StringTokenizer(exportPackage, ",");
+		SortedSet<String> exportPackageSet = new TreeSet<String>();
+		while (st.hasMoreTokens()) {
+			exportPackageSet.add(st.nextToken());
+		}
+
+		// check, whether there are new remote packages that do not yet exist locally.
+		Set<String> remotePackages = JFireRCDLDelegate.sharedInstance().getPublishedRemotePackages();
+		StringBuffer newExportPackage = null;
+		for (String pkg : remotePackages) {
+			if (!exportPackageSet.contains(pkg)) {
+				if (newExportPackage == null)
+					newExportPackage = new StringBuffer(exportPackage);
+
+				newExportPackage.append(',');
+				newExportPackage.append(pkg);
+			}
+		}
+
+		if (newExportPackage == null) {
+			System.out.println("There are NO new remote packages. No need to rewrite the MANIFEST.MF!");
+			return false;
+		}
+
+		System.out.println("There are new remote packages. We must rewrite the MANIFEST.MF!");
+		manifest.getMainAttributes().put(new Attributes.Name("Export-Package"), newExportPackage.toString());
+//		OutputStream out = new FileOutputStream(new File(manifestFile.getAbsolutePath()+".new"));
+		OutputStream out = new FileOutputStream(manifestFile);
+		try {
+			manifest.write(out);
+		} finally {
+			out.close();
+		}
+
+		return true;
+	}
 }
