2bc732d3cec0c437dcfcef3a5c63c371fb76994d
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-01-05 20:54:22 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.intro.ui@9606 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/intro/SwitchToTradePerspectiveAction.java
@@ -0,0 +1,13 @@
+package org.nightlabs.jfire.intro;
+
+
+/**
+ * @author Marc Klinger - marc[at]nightlabs[dot]de
+ */
+public class SwitchToTradePerspectiveAction extends SwitchToPerspectiveAction
+{
+	public SwitchToTradePerspectiveAction()
+	{
+		super("org.nightlabs.jfire.trade.ui.TradePerspective");
+	}
+}
