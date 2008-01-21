cdbdff6b90f75ea7e69236a0c30e425863dda699
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-01-21 19:16:03 +0000

    https://www.jfire.org/modules/bugs/view.php?id=442 : By default, 4 organisations are initialised simultaneously during server start. https://www.jfire.org/modules/bugs/view.php?id=443 : We now provide the possibility to log userID, organisationID or completeUserID (${userID}@${organisationID}).
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.ui@9749 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/ui/security/SecurityReflectorClient.java
+++ src/org/nightlabs/jfire/base/ui/security/SecurityReflectorClient.java
@@ -6,6 +6,7 @@ import javax.naming.InitialContext;
 
 import org.apache.log4j.Logger;
 import org.nightlabs.jfire.base.ui.login.Login;
+import org.nightlabs.jfire.security.NoUserException;
 import org.nightlabs.jfire.security.SecurityReflector;
 
 /**
@@ -19,29 +20,34 @@ extends SecurityReflector
 	private static final Logger logger = Logger.getLogger(SecurityReflectorClient.class);
 
 	@Override
-	public UserDescriptor _getUserDescriptor() {
+	public UserDescriptor _getUserDescriptor() throws NoUserException {
 		if (logger.isDebugEnabled())
 			logger.debug("_getUserDescriptor: enter"); //$NON-NLS-1$
 
-		Login l = Login.sharedInstance();
+		Login l;
+		try {
+			l = Login.getLogin();
+		} catch (Exception e) {
+			throw new NoUserException(e);
+		}
 		return new UserDescriptor(l.getOrganisationID(), l.getUserID(), l.getSessionID());
 	}
 
 	@Override
-	public InitialContext _createInitialContext() {
+	public InitialContext _createInitialContext() throws NoUserException {
 		try {
 			return Login.getLogin().createInitialContext();
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			throw new NoUserException(e);
 		}
 	}
 
 	@Override
-	public Properties _getInitialContextProperties() {
+	public Properties _getInitialContextProperties() throws NoUserException {
 		try {
 			return Login.getLogin().getInitialContextProperties();
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			throw new NoUserException(e);
 		}
 	}
 }
