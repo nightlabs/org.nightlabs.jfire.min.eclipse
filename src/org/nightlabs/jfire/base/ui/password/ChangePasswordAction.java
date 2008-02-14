7f5031e6d763a0ffa07cdd4240a5e9acff6611a6
tobias <tobias@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-02-14 16:16:48 +0000

    - Changed handling of password  - Changed the admin GUI to change passwords  - Changed the GUI to change someone's own password
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.ui@9940 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/ui/password/ChangePasswordAction.java
+++ src/org/nightlabs/jfire/base/ui/password/ChangePasswordAction.java
@@ -1,9 +1,22 @@
 package org.nightlabs.jfire.base.ui.password;
 
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.nightlabs.base.ui.dialog.ChangePasswordDialog;
+import org.nightlabs.base.ui.util.RCPUtil;
+import org.nightlabs.jfire.base.ui.login.Login;
+import org.nightlabs.jfire.security.SecurityReflector;
+import org.nightlabs.jfire.security.UserLocal;
+import org.nightlabs.jfire.security.UserManager;
+import org.nightlabs.jfire.security.UserManagerUtil;
 
 public class ChangePasswordAction implements IWorkbenchWindowActionDelegate {
 
@@ -18,7 +31,54 @@ public class ChangePasswordAction implements IWorkbenchWindowActionDelegate {
 	}
 
 	public void run(IAction arg0) {
-		ChangePasswordDialog.openDialog();
+		InputDialog dialog = new InputDialog(RCPUtil.getActiveShell(), "Enter Password", "Enter your current password to proceed.", "", null) {
+			@Override
+			protected void okPressed() {
+				try {
+					if (Login.getLogin().getPassword().equals(getValue()))
+						super.okPressed();
+					else {
+						Thread.sleep(2000);
+						MessageDialog.openError(RCPUtil.getActiveShell(), "Wrong password", "The entered password is not correct.\n\nPlease try again.");
+					}
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			};
+			
+			@Override
+			protected Control createDialogArea(Composite parent) {
+				Control dialogArea = super.createDialogArea(parent);
+				getText().setEchoChar('*');
+				return dialogArea;
+			}
+		};
+		
+		if (dialog.open() != Window.OK)
+			return;
+		
+		IInputValidator newPasswordValidator = new IInputValidator() {
+			@Override
+			public String isValid(String password) {
+				if (password.length() < UserLocal.MIN_PASSWORD_LENGTH)
+					return "Minimum password length: " + UserLocal.MIN_PASSWORD_LENGTH;
+				if (password.matches("\\*+"))
+					return "Passwords only consisting of '*' are not valid.";
+
+				return null;
+			}
+		};
+		String newPassword = ChangePasswordDialog.openDialog(RCPUtil.getActiveShell(), newPasswordValidator, null);
+		try {
+			UserManager um = UserManagerUtil.getHome(SecurityReflector.getInitialContextProperties()).create();
+			um.setUserPassword(newPassword);
+			Login.sharedInstance().setPassword(newPassword);
+			MessageDialog.openInformation(RCPUtil.getActiveShell(), "Password changed", "Your password has been changed successfully.");
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+//		ChangePasswordDialog.openDialog();
 	}
 
 	public void selectionChanged(IAction arg0, ISelection arg1) {
