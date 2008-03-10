9f2c906c11e25659759190b663f9a1015b515fe0
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-03-10 18:05:14 +0000

    minor changes
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.ui@10070 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/ui/search/AbstractJDOSelectionZoneActionHandler.java
+++ src/org/nightlabs/jfire/base/ui/search/AbstractJDOSelectionZoneActionHandler.java
@@ -5,10 +5,12 @@ package org.nightlabs.jfire.base.ui.search;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 import org.nightlabs.base.ui.notification.SelectionManager;
 import org.nightlabs.base.ui.search.AbstractSelectionZoneActionHandler;
 import org.nightlabs.jdo.NLJDOHelper;
+import org.nightlabs.jdo.ObjectID;
 import org.nightlabs.notification.NotificationEvent;
 
 /**
@@ -22,8 +24,17 @@ extends AbstractSelectionZoneActionHandler
 	public void run() {
 		Collection selectedObjects = getSearchResultProvider().getSelectedObjects();
 		if (selectedObjects != null) {
-			// TODO: before use NLJDOHelper.getObjectIDSet check if maybe already ObjectIDs are contained  
-			Collection selectedObjectIDs = NLJDOHelper.getObjectIDSet(selectedObjects);
+			Collection<ObjectID> selectedObjectIDs = new ArrayList<ObjectID>();
+			// before use NLJDOHelper.getObjectIDSet check if maybe already ObjectIDs are contained
+			for (Iterator it = selectedObjects.iterator(); it.hasNext(); ) {
+				Object o = it.next();
+				if (o instanceof ObjectID) {
+					selectedObjectIDs.add((ObjectID)o);
+				}
+			}
+			if (selectedObjectIDs.isEmpty()) {
+				selectedObjectIDs = NLJDOHelper.getObjectIDSet(selectedObjects);
+			}
 			Collection<Class> subjectClassesToClear = new ArrayList<Class>();
 			subjectClassesToClear.add(getSearchResultProvider().getFactory().getResultTypeClass());
 			if (selectedObjects != null) {
