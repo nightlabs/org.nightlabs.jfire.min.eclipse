9a678f8ba850cfe1cd3082649a48d29df7307e28
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-06-04 18:32:11 +0000

    refactored IProductTypeQuickListFilter getQuery to getQueryCollection + changed ProductTypeGroupDAO to use ProductTypeGroupSearchResult and ProductTypeGroupIDSearchResult
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.ui@10688 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/ui/overview/search/QueryFilterDialog.java
+++ src/org/nightlabs/jfire/base/ui/overview/search/QueryFilterDialog.java
@@ -13,7 +13,9 @@ import org.nightlabs.base.ui.composite.XComposite;
 import org.nightlabs.base.ui.composite.XComposite.LayoutDataMode;
 import org.nightlabs.base.ui.composite.XComposite.LayoutMode;
 import org.nightlabs.eclipse.ui.dialog.ResizableTitleAreaDialog;
+import org.nightlabs.jdo.query.AbstractSearchQuery;
 import org.nightlabs.jdo.query.DefaultQueryProvider;
+import org.nightlabs.jdo.query.QueryCollection;
 
 /**
  * @author Daniel Mazurek - daniel [at] nightlabs [dot] de
@@ -23,27 +25,33 @@ public class QueryFilterDialog
 extends ResizableTitleAreaDialog 
 {
 	private String scope;
-	private Class<?> targetType;
+	private QueryCollection<? extends AbstractSearchQuery> queryCollection;
+//	private Set<AbstractQueryFilterComposite> filterComposites;
 	
 	/**
 	 * @param parentShell
 	 */
-	public QueryFilterDialog(Shell parentShell, String scope, Class<?> targetType) {
+	public QueryFilterDialog(Shell parentShell, String scope, 
+			QueryCollection<? extends AbstractSearchQuery> queryCollection) 
+	{
 		super(parentShell, null);
 		this.scope = scope;
-		this.targetType = targetType;
+		this.queryCollection = queryCollection;
 	}
 
 	@Override
 	protected Control createDialogArea(Composite parent) 
 	{
 		Composite wrapper = new XComposite(parent, SWT.NONE);
+		Class<?> targetType = queryCollection.getResultClass();
 		SortedSet<QueryFilterFactory> factories = QueryFilterFactoryRegistry.sharedInstance().getQueryFilterCompositesFor(
 				scope, targetType);
 		if (factories != null) {
 			for (QueryFilterFactory factory : factories) {
-				factory.createQueryFilter(wrapper, SWT.NONE, LayoutMode.ORDINARY_WRAPPER, 
-						LayoutDataMode.GRID_DATA, new DefaultQueryProvider(targetType));
+				AbstractQueryFilterComposite filterComp = factory.createQueryFilter(wrapper, SWT.NONE, 
+						LayoutMode.ORDINARY_WRAPPER, LayoutDataMode.GRID_DATA, new DefaultQueryProvider(targetType));
+//				filterComposites.add(filterComp);
+				filterComp.getQueryProvider().loadQueries(queryCollection);
 			}
 		}
 		return wrapper;
