6196bc8bb56ae1c8859f75537a036cedf934b1d4
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-06-07 00:13:30 +0000

    bugfixes and role/role-group configuration
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.admin.ui@10738 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/admin/ui/editor/user/RoleGroupTableViewer.java
+++ src/org/nightlabs/jfire/base/admin/ui/editor/user/RoleGroupTableViewer.java
@@ -3,16 +3,24 @@ package org.nightlabs.jfire.base.admin.ui.editor.user;
 import java.util.Collection;
 import java.util.Collections;
 
+import org.apache.log4j.Logger;
 import org.eclipse.jface.viewers.TableLayout;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseMoveListener;
+import org.eclipse.swt.events.MouseTrackListener;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 import org.nightlabs.base.ui.layout.WeightedTableLayout;
 import org.nightlabs.base.ui.notification.IDirtyStateManager;
 import org.nightlabs.base.ui.resource.SharedImages;
@@ -22,9 +30,10 @@ import org.nightlabs.jfire.base.admin.ui.BaseAdminPlugin;
 import org.nightlabs.jfire.base.admin.ui.resource.Messages;
 import org.nightlabs.jfire.security.RoleGroup;
 import org.nightlabs.util.NLLocale;
-
 public class RoleGroupTableViewer extends TableViewer
 {
+	private static final Logger logger = Logger.getLogger(RoleGroupTableViewer.class);
+
 	/**
 	 * Content provider for role groups.
 	 */
@@ -140,11 +149,199 @@ public class RoleGroupTableViewer extends TableViewer
 		getTable().setLayout(tlayout);
 		getTable().setHeaderVisible(true);
 
-//		new TableColumn(table, SWT.LEFT).setText("Name");
-//		new TableColumn(table, SWT.LEFT).setText("Description");
+		getTable().addDisposeListener(new DisposeListener() {
+			@Override
+			public void widgetDisposed(DisposeEvent event) {
+				disposeToolTip();
+			}
+		});
+
+		getTable().addMouseTrackListener(new MouseTrackListener() {
+			@Override
+			public void mouseEnter(MouseEvent event) {
+//				if (logger.isDebugEnabled())
+//					logger.debug("table.MouseTrackListener.mouseEnter");
+			}
+			@Override
+			public void mouseExit(MouseEvent event) {
+				if (logger.isDebugEnabled())
+					logger.debug("table.MouseTrackListener.mouseExit: clearing tool tip");
+
+				disposeToolTip();
+			}
+			@Override
+			public void mouseHover(MouseEvent event) {
+				if (logger.isDebugEnabled())
+					logger.debug("table.MouseTrackListener.mouseHover: creating tool tip");
+
+				createToolTip(new Point(event.x, event.y));
+			}
+		});
+
+		getTable().addMouseMoveListener(new MouseMoveListener() {
+			@Override
+			public void mouseMove(MouseEvent event) {
+				if (logger.isDebugEnabled())
+					logger.debug("table.MouseMoveListener.mouseMove: clearing tool tip");
+
+				disposeToolTip();
+			}
+		});
 
 		setContentProvider(new RoleGroupsContentProvider());
 		setLabelProvider(new RoleGroupsLabelProvider(this));
+//		toolTipManagerThread.start();
+	}
+
+//	private ToolTip toolTip;
+//
+//	private Thread toolTipManagerThread = new Thread() {
+//		{
+//			setDaemon(true);
+//		}
+//
+//		@Override
+//		public void run() {
+//			while (true) {
+//				try {
+//					Thread.sleep(5000);
+//				} catch (InterruptedException e) {
+//					// ignore
+//				}
+//
+//				if (getTable().isDisposed()) {
+//					getTable().getDisplay().asyncExec(new Runnable() {
+//						public void run() {
+//							if (toolTip != null) {
+//								toolTip.setMessage(" ");
+//								toolTip.dispose();
+//								toolTip = null;
+//							}
+//						}
+//					});
+//
+//					return;
+//				}
+//
+//				getTable().getDisplay().asyncExec(new Runnable() {
+//					public void run() {
+//						if (lastToolTipRequest == null)
+//							return;
+//
+//						if (toolTip != null) {
+//							toolTip.setMessage(" ");
+//							toolTip.dispose();
+//							toolTip = null;
+//							return; // wait for the next loop
+//						}
+//
+//						if (lastToolTipRequest.getPosition() != null && lastToolTipRequest.getText() != null) {
+//							if (logger.isDebugEnabled())
+//								logger.debug("toolTipManagerThread: creating tool tip: " + lastToolTipRequest.getText());
+//
+//							toolTip = new ToolTip(getTable().getShell(), SWT.ICON_INFORMATION); // SWT.BALLOON);
+//							toolTip.addDisposeListener(new DisposeListener() {
+//								@Override
+//								public void widgetDisposed(DisposeEvent event) {
+//									if (logger.isDebugEnabled())
+//										logger.debug("toolTip.DisposeListener: tool tip widget disposed");
+//								}
+//							});
+//
+//							toolTip.setAutoHide(false);
+//							toolTip.setMessage(lastToolTipRequest.getText());
+//							toolTip.setLocation(lastToolTipRequest.getPosition());
+//							toolTip.setVisible(true);
+//						}
+//
+//						lastToolTipRequest = null;
+//					}
+//				});
+//			}
+//		}
+//	};
+
+// This fucking ToolTip doesn't work in a clean way in GTK+ (Linux)! Even this workaround with the thread didn't help. It seems, there
+// is only one shared tool-tip-GTK-widget (heavy weight) that's somehow shared and accessed by the table itself and by my manual code
+// (i.e. my ToolTip instance). It works pretty well with SWT.BALLOON. Shall we use this instead? How does it look on Windows?
+
+	private void disposeToolTip() {
+		lastToolTipRequest = new ToolTipRequest(null, null);
+		getTable().setToolTipText("");
+//		if (roleGroupDescriptionToolTip != null) {
+//			roleGroupDescriptionToolTip.setMessage("");
+//			roleGroupDescriptionToolTip.setVisible(false);
+//			roleGroupDescriptionToolTip.dispose();
+//			roleGroupDescriptionToolTip = null;
+//
+//			// SWT seems to use a persistent object in the background that's not disposed - thus I try this workaround, but it doesn't work 100% :-(
+//			ToolTip workaround = new ToolTip(getTable().getShell(), SWT.ICON_INFORMATION);
+//			workaround.setMessage("");
+//			workaround.setVisible(true);
+//			workaround.setVisible(false);
+//			workaround.dispose();
+//		}
+	}
+
+	private volatile ToolTipRequest lastToolTipRequest = null;
+
+	private static class ToolTipRequest {
+		private Point position;
+		private String text;
+
+		public ToolTipRequest(Point position, String text)
+		{
+			this.position = position;
+			this.text = text;
+		}
+		public Point getPosition() {
+			return position;
+		}
+		public String getText() {
+			return text;
+		}
+	}
+
+	private void createToolTip(final Point mousePositionRelativeToTable) {
+		TableItem tableItem = getTable().getItem(mousePositionRelativeToTable);
+		Object o = tableItem == null ? null :tableItem.getData();
+		if (!(o instanceof RoleGroup)) {
+			if (logger.isDebugEnabled())
+				logger.debug("showToolTip: no role-group => disposing tool tip");
+
+			disposeToolTip();
+			return;
+		}
+
+		final RoleGroup roleGroup = (RoleGroup) o;
+		final String toolTipText = roleGroup.getDescription().getText();
+
+//		if (logger.isDebugEnabled())
+//			logger.debug("createToolTip: disposing tool tip before setting it to: " + toolTipText);
+
+		// Since we cannot relocate it (that only works with the ToolTip if it has another style - SWT.BALOON - which has other problems),
+		// we destroy it and recreate it.
+		disposeToolTip();
+
+		if (logger.isDebugEnabled())
+			logger.debug("createToolTip: setting tool tip to: " + toolTipText);
+
+		lastToolTipRequest = new ToolTipRequest(getTable().toDisplay(mousePositionRelativeToTable), toolTipText);
+
+		getTable().setToolTipText(toolTipText);
+//		roleGroupDescriptionToolTip = new ToolTip(getTable().getShell(), SWT.ICON_INFORMATION); // SWT.BALLOON);
+//		roleGroupDescriptionToolTip.addDisposeListener(new DisposeListener() {
+//			@Override
+//			public void widgetDisposed(DisposeEvent event) {
+//				if (logger.isDebugEnabled())
+//					logger.debug("roleGroupDescriptionToolTip.DisposeListener: tool tip widget disposed");
+//			}
+//		});
+//
+//		roleGroupDescriptionToolTip.setAutoHide(false);
+//		roleGroupDescriptionToolTip.setMessage(toolTipText);
+//		roleGroupDescriptionToolTip.setLocation(getTable().toDisplay(mousePositionRelativeToTable));
+//		roleGroupDescriptionToolTip.setVisible(true);
 	}
 
 	private CheckboxEditingSupport<RoleGroup> checkboxEditingSupport = new CheckboxEditingSupport<RoleGroup>(this) {
