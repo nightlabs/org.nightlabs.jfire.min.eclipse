eab617b3501bb8908ac8a7f511d84003de674ca8
marc <marc@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-07-02 17:35:00 +0000

    language switch
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.ui@11213 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/ui/language/SwitchLanguageDialog.java
+++ src/org/nightlabs/jfire/base/ui/language/SwitchLanguageDialog.java
@@ -1,48 +1,59 @@
-/**
- * 
- */
 package org.nightlabs.jfire.base.ui.language;
 
-import org.eclipse.jface.dialogs.Dialog;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.nightlabs.base.ui.composite.XComposite;
 import org.nightlabs.base.ui.composite.XComposite.LayoutMode;
 import org.nightlabs.base.ui.language.LanguageChooserList;
 import org.nightlabs.base.ui.language.LanguageManager;
+import org.nightlabs.eclipse.ui.dialog.ResizableTitleAreaDialog;
 import org.nightlabs.jfire.base.ui.resource.Messages;
 import org.nightlabs.language.LanguageCf;
-import org.nightlabs.util.NLLocale;
+import org.nightlabs.util.IOUtil;
 
 /**
  * @author Alexander Bieber <!-- alex [at] nightlabs [dot] de -->
- *
+ * @author Marc Klinger - marc[at]nightlabs[dot]de
  */
-public class SwitchLanguageDialog extends Dialog {
+public class SwitchLanguageDialog extends ResizableTitleAreaDialog {
 
 	private LanguageChooserList languageChooser;
-	
+
 	/**
-	 * @param parentShell
+	 * Create a new SwitchLanguageDialog instance.
+	 * @param parentShell The parent shell
 	 */
 	public SwitchLanguageDialog(Shell parentShell) {
-		super(parentShell);
+		super(parentShell, Messages.getBundle());
 		setShellStyle(getShellStyle() | SWT.RESIZE);
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.Dialog#createDialogArea(org.eclipse.swt.widgets.Composite)
+	 */
 	@Override
 	protected Control createDialogArea(Composite parent) {
+		setTitle(Messages.getString("org.nightlabs.jfire.base.ui.language.SwitchLanguageDialog.titleAreaTitle")); //$NON-NLS-1$
+		setMessage(Messages.getString("org.nightlabs.jfire.base.ui.language.SwitchLanguageDialog.titleAreaMessage")); //$NON-NLS-1$
+
 		Composite wrapper = new XComposite(parent, SWT.NONE, LayoutMode.ORDINARY_WRAPPER);
-		Label l = new Label(wrapper, SWT.WRAP);
-		l.setText(Messages.getString("org.nightlabs.jfire.base.ui.language.SwitchLanguageDialog.label.selectLanguage")); //$NON-NLS-1$
-		l.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+//		Label l = new Label(wrapper, SWT.WRAP);
+//		l.setText(Messages.getString("org.nightlabs.jfire.base.ui.language.SwitchLanguageDialog.label.selectLanguage")); //$NON-NLS-1$
+//		l.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		languageChooser = new LanguageChooserList(wrapper);
 		languageChooser.setLayoutData(new GridData(GridData.FILL_BOTH));
 		languageChooser.addDoubleClickListener(new IDoubleClickListener() {
@@ -54,24 +65,107 @@ public class SwitchLanguageDialog extends Dialog {
 		return wrapper;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.Dialog#okPressed()
+	 */
 	@Override
 	protected void okPressed() {
 		LanguageCf langCf = languageChooser.getLanguage();
+		String languageID = langCf.getLanguageID();
 		super.okPressed();
-		if (NLLocale.getDefault().getLanguage().equals(langCf.getLanguageID()))
+		if (LanguageManager.sharedInstance().getCurrentLanguageID().equals(languageID))
 			return; // nothing to do, same language
-		LanguageManager.sharedInstance().setLanguageID(langCf.getLanguageID());
-		PlatformUI.getWorkbench().restart();
+		switchLanguage(languageID);
+		boolean doRestart = MessageDialog.openQuestion(getShell(), Messages.getString("org.nightlabs.jfire.base.ui.language.SwitchLanguageDialog.restartQuestionTitle"), Messages.getString("org.nightlabs.jfire.base.ui.language.SwitchLanguageDialog.restartQuestionMessage")); //$NON-NLS-1$ //$NON-NLS-2$
+		if(doRestart)
+			PlatformUI.getWorkbench().restart();
 	}
-	
+
+	private void switchLanguage(String languageID)
+	{
+		LanguageManager.sharedInstance().setLanguageID(languageID);
+
+		try {
+			String launcher = null;
+			String eclipseCommands = System.getProperty("eclipse.commands");
+			if(eclipseCommands != null) {
+				String[] lines = eclipseCommands.split("\n");
+				if(lines != null) {
+					for (int i=0; i<lines.length; i++) {
+						if(lines[i].equals("-launcher") && lines.length > i+1)
+							launcher = lines[i+1];
+					}
+				}
+			}
+			if(launcher != null) {
+				String launcherBaseName = launcher;
+				if(launcherBaseName.toLowerCase().endsWith(".exe"))
+					launcherBaseName = launcherBaseName.substring(0, launcherBaseName.length()-".exe".length());
+				String ini = launcher+".ini";
+				File iniFile = new File(ini);
+				if(!iniFile.canWrite())
+					throw new IOException("Cannot write to ini file: "+iniFile.getAbsolutePath());
+
+				// backup:
+				File backupFile = new File(ini+".bak");
+				int count = 1;
+				while(backupFile.exists()) {
+					backupFile = new File(ini+".bak"+count);
+					count++;
+				}
+				IOUtil.copyFile(iniFile, backupFile);
+
+				// read and update contents in memory
+				StringBuffer newContents = new StringBuffer();
+				BufferedReader fileReader = new BufferedReader(new FileReader(iniFile));
+				String line = null;
+				boolean replaceNextLine = false;
+				boolean foundEntry = false;
+				boolean foundVMArgs = false;
+				while((line = fileReader.readLine()) != null) {
+					if(!foundVMArgs && line.trim().equals("-vmargs")) {
+						foundVMArgs = true;
+					}
+					if(!foundVMArgs && replaceNextLine) {
+						newContents.append(languageID);
+						replaceNextLine = false;
+					} else {
+						if(!foundVMArgs && line.trim().equals("-nl")) {
+							replaceNextLine = true;
+							foundEntry = true;
+						}
+						newContents.append(line);
+					}
+					newContents.append('\n');
+				}
+				fileReader.close();
+				if(!foundEntry) {
+					newContents.insert(0, '\n');
+					newContents.insert(0, languageID);
+					newContents.insert(0, "-nl\n");
+				}
+				// write new contents
+				FileWriter fileWriter = new FileWriter(iniFile);
+				fileWriter.write(newContents.toString());
+				fileWriter.close();
+			}
+		} catch(Exception e) {
+			Logger.getLogger(SwitchLanguageDialog.class).error("Error updating launcher ini file", e);
+			MessageDialog.openWarning(getShell(), "Error switching language", "The language configuration could only partially be saved. The application may appear to be in multiple languages. Please contact your administrator to change the application language.");
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.window.Window#configureShell(org.eclipse.swt.widgets.Shell)
+	 */
 	@Override
 	protected void configureShell(Shell newShell) {
 		super.configureShell(newShell);
 		newShell.setText(Messages.getString("org.nightlabs.jfire.base.ui.language.SwitchLanguageDialog.window.title")); //$NON-NLS-1$
-		newShell.setSize(300, 300);
+		//newShell.setSize(300, 300);
 //		setToCenteredLocationPreferredSize(newShell, 300, 300);
 	}
 
-	
-	
+
+
 }
