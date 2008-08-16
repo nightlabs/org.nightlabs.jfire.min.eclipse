e29b62e719125d863a050130d37adb3168adaad0
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2008-08-16 15:16:13 +0000

    documentation DataBlockEditor
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.ui@11842 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/ui/prop/edit/blockbased/IDataBlockEditorComposite.java
+++ src/org/nightlabs/jfire/base/ui/prop/edit/blockbased/IDataBlockEditorComposite.java
@@ -3,24 +3,55 @@ package org.nightlabs.jfire.base.ui.prop.edit.blockbased;
 import org.eclipse.swt.events.DisposeListener;
 import org.nightlabs.jfire.base.ui.prop.edit.DataFieldEditorChangedListener;
 import org.nightlabs.jfire.prop.DataBlock;
+import org.nightlabs.jfire.prop.DataField;
 import org.nightlabs.jfire.prop.IStruct;
 
+/**
+ * This interface is used by {@link AbstractDataBlockEditor} to implement 
+ * the {@link DataBlockEditor} interface. It should show the UI to edit
+ * the {@link DataField}s of a {@link DataBlock}.
+ * <p>
+ * This interface is intended to be implemented by subclassing 
+ * {@link AbstractDataBlockEditorComposite} rather than directly.
+ * </p>
+ * 
+ * @author Alexander Bieber <!-- alex [AT] nightlabs [DOT] de --> 
+ */
 public interface IDataBlockEditorComposite {
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.nightlabs.jfire.base.ui.prop.edit.blockbased.AbstractDataBlockEditor#refresh(org.nightlabs.jfire.prop.IStruct, org.nightlabs.jfire.prop.DataBlock)
+	/**
+	 * Refresh the UI with the data of the given {@link DataBlock}.
+	 * 
+	 * @param struct The {@link IStruct} the given {@link DataBlock} was built from.
+	 * @param dataBlock The {@link DataBlock} to show.
 	 */
 	void refresh(IStruct struct, DataBlock dataBlock);
-	
+	/**
+	 * Here the changes made in the UI should be reflected to the {@link DataBlock}
+	 * set with {@link #refresh(IStruct, DataBlock)}.
+	 */
 	void updatePropertySet();
-	
+
+	/**
+	 * 
+	 * @param validationResultManager
+	 */
 	void setValidationResultManager(IValidationResultManager validationResultManager);
 
+	/**
+	 * Add the given listener that will be notified of changes in the UI.
+	 * @param listener The listener to add.
+	 */
 	void addDataFieldEditorChangeListener(DataFieldEditorChangedListener listener);
-	
+	/**
+	 * Remove the given listener.
+	 * @param listener The listener to remove.
+	 */
 	void removeDataFieldEditorChangeListener(DataFieldEditorChangedListener listener);
-	
+	/**
+	 * Add a {@link DisposeListener} to this {@link IDataBlockEditorComposite}.
+	 * 
+	 * @param listener The listener to add.
+	 */
 	void addDisposeListener(DisposeListener listener);
-
 }
