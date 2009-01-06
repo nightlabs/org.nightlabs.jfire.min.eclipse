3d98f134308a38b62043f0844da418fb1a52bda6
marius <marius@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-01-06 16:02:04 +0000

    - added a more or less useful tooltip for AbstractArticleContainerEditors as well as the OverviewEditor - removed a buggy debug output of AbstractJDOQuery(s) - degraded some anoying info output to trace/debug
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.ui@13094 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/ui/overview/OverviewEntryEditorInput.java
+++ src/org/nightlabs/jfire/base/ui/overview/OverviewEntryEditorInput.java
@@ -1,17 +1,17 @@
 package org.nightlabs.jfire.base.ui.overview;
 
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.internal.part.NullEditorInput;
+import org.eclipse.ui.IPersistableElement;
 
 /**
  * {@link IEditorInput} to be used with {@link OverviewEntryEditor}.
- * 
+ *
  * @author Daniel.Mazurek [at] NightLabs [dot] de
  *
  */
 public class OverviewEntryEditorInput
-//implements IEditorInput
-extends NullEditorInput
+	implements IEditorInput
 {
 
 	public OverviewEntryEditorInput(Entry entry) {
@@ -26,7 +26,7 @@ extends NullEditorInput
 	public Entry getEntry() {
 		return entry;
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 * <p>
@@ -40,15 +40,51 @@ extends NullEditorInput
 	{
 		if (obj == null)
 			return false;
-		
+
 		if (!(obj instanceof OverviewEntryEditorInput))
 			return false;
-	
+
 		OverviewEntryEditorInput input = (OverviewEntryEditorInput) obj;
 		if (entry.getEntryFactory().getID().equals(input.getEntry().getEntryFactory().getID()))
 			return true;
-		
+
 		return false;
 	}
-		
+
+	@Override
+	public boolean exists()
+	{
+		return false;
+	}
+
+	@Override
+	public ImageDescriptor getImageDescriptor()
+	{
+		return null;
+	}
+
+	@Override
+	public String getName()
+	{
+		return entry.getEntryFactory().getName();
+	}
+
+	@Override
+	public IPersistableElement getPersistable()
+	{
+		return null;
+	}
+
+	@Override
+	public String getToolTipText()
+	{
+		return getName();
+	}
+
+	@Override
+	public Object getAdapter(Class adapter)
+	{
+		return null;
+	}
+
 }
