7250db9ccd7cef519f45b570ff1620bb735e7767
nozkiller <nozkiller@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-03-23 18:46:51 +0000

    added overtaking of modified quickSearchText in personSearchWizward + implemented PersonEditWizard in HeadervendorCustmerComposite
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jdo.query.ui@13681 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jdo/ui/search/EarlySearchFilterProvider.java
+++ src/org/nightlabs/jdo/ui/search/EarlySearchFilterProvider.java
@@ -3,6 +3,8 @@
  */
 package org.nightlabs.jdo.ui.search;
 
+import org.eclipse.swt.events.ModifyListener;
+
 /**
  * {@link SearchFilterProvider} that accepts a
  * String to do an early search with the user
@@ -14,4 +16,14 @@ package org.nightlabs.jdo.ui.search;
 public interface EarlySearchFilterProvider extends SearchFilterProvider {
 
 	void setEarlySearchText(String earlySearchText);
+	
+	/**
+	 * Returns the current search string. 
+	 * @return the current search string
+	 */
+	public String getSearchText();
+	
+	public void addSearchTextModifyListener(ModifyListener listener);
+	
+	public void removeSearchTextModifyListener(ModifyListener listener);
 }
