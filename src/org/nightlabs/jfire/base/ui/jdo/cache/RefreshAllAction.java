8d30b5d8a8f34923683ca7286880d53add5e9223
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-03-26 08:18:54 +0000

    https://www.jfire.org/modules/bugs/view.php?id=1096
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.ui@13700 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/ui/jdo/cache/RefreshAllAction.java
+++ src/org/nightlabs/jfire/base/ui/jdo/cache/RefreshAllAction.java
@@ -1,34 +1,14 @@
 package org.nightlabs.jfire.base.ui.jdo.cache;
 
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.nightlabs.jfire.base.jdo.cache.Cache;
+import org.nightlabs.jfire.base.ui.login.action.LSDWorkbenchWindowActionDelegate;
 
 public class RefreshAllAction
-implements IWorkbenchWindowActionDelegate
+extends LSDWorkbenchWindowActionDelegate
 {
-	@Override
-	public void dispose() {
-		// nothing to do
-	}
-
-	private IWorkbenchWindow window;
-
-	@Override
-	public void init(IWorkbenchWindow window) {
-		this.window = window;
-	}
-
 	@Override
 	public void run(IAction action) {
 		Cache.sharedInstance().refreshAll();
 	}
-
-	@Override
-	public void selectionChanged(IAction action, ISelection selection)
-	{
-	}
-
 }
