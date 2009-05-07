ca2c6b96202292ea2e3ce836088f0bdbfba83f1d
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-05-07 13:03:24 +0000

    Implemented ActiveEntityEditor to generically handle loading of editor title.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.ui@14471 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/base/ui/entity/editor/ActiveEntityEditor.java
@@ -0,0 +1,219 @@
+package org.nightlabs.jfire.base.ui.entity.editor;
+
+import java.util.Collection;
+
+import javax.jdo.JDOHelper;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IPartListener2;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartReference;
+import org.nightlabs.base.ui.entity.editor.EntityEditor;
+import org.nightlabs.base.ui.job.Job;
+import org.nightlabs.base.ui.notification.NotificationAdapterJob;
+import org.nightlabs.base.ui.part.PartAdapter;
+import org.nightlabs.base.ui.util.RCPUtil;
+import org.nightlabs.jfire.base.jdo.notification.JDOLifecycleManager;
+import org.nightlabs.jfire.jdo.notification.DirtyObjectID;
+import org.nightlabs.notification.NotificationEvent;
+import org.nightlabs.notification.NotificationListener;
+import org.nightlabs.progress.ProgressMonitor;
+
+public abstract class ActiveEntityEditor
+extends EntityEditor
+{
+	public ActiveEntityEditor()
+	{
+		workbenchPage = RCPUtil.getActiveWorkbenchPage();
+		workbenchPage.addPartListener(partListenerEditorClosed);
+	}
+
+	private IPartListener2 partListenerEditorClosed = new PartAdapter() {
+		@Override
+		public void partClosed(IWorkbenchPartReference reference) {
+			if (workbenchPage == null) // Should never happen that this listener is triggered twice, but better safe than sorry.
+				return;
+
+			IWorkbenchPart part = reference.getPart(false);
+			if (ActiveEntityEditor.this != part)
+				return;
+
+			removeChangeListener();
+			workbenchPage.removePartListener(partListenerEditorClosed);
+			partListenerEditorClosed = null;
+			workbenchPage = null;
+		}
+	};
+
+	private IWorkbenchPage workbenchPage;
+	private NotificationListener changeListener = null;
+	private Class<?> entityClass = null;
+	private Object entityObjectID = null;
+
+	private synchronized void addChangeListener(Object entity)
+	{
+		removeChangeListener();
+
+		if (entity == null)
+			return;
+
+		changeListener = new NotificationAdapterJob() {
+			@Override
+			public void notify(NotificationEvent event) {
+				Object eoid = entityObjectID;
+				if (eoid == null)
+					return;
+
+				@SuppressWarnings("unchecked")
+				Collection<? extends DirtyObjectID> c = event.getSubjects();
+
+				for (DirtyObjectID dirtyObjectID : c) {
+					if (eoid.equals(dirtyObjectID.getObjectID())) {
+						scheduleLoadTitleJob();
+						break;
+					}
+				}
+			}
+		};
+		entityClass = entity.getClass();
+		entityObjectID = JDOHelper.getObjectId(entity);
+		if (entityObjectID == null)
+			throw new IllegalStateException("Entity has no object-id assigned! " + entity);
+
+		JDOLifecycleManager.sharedInstance().addNotificationListener(
+				entityClass, changeListener
+		);
+	}
+
+	private synchronized void removeChangeListener()
+	{
+		if (entityClass == null)
+			return;
+
+		JDOLifecycleManager.sharedInstance().removeNotificationListener(
+				entityClass, changeListener
+		);
+
+		entityObjectID = null;
+		changeListener = null;
+		entityClass = null;
+	}
+
+	@Override
+	public String getTitle() {
+		if (entityTitle != null)
+			return entityTitle;
+
+		if(getEditorInput() == null)
+			return super.getTitle();
+
+		if (loadTitleJob == null)
+			scheduleLoadTitleJob();
+
+		return super.getTitle();
+	}
+
+	private String entityTitle = null;
+
+	private volatile Job loadTitleJob = null;
+	private void scheduleLoadTitleJob()
+	{
+		Job job = new Job("Loading entity") {
+			@Override
+			protected IStatus run(ProgressMonitor monitor) throws Exception {
+				final Job thisJob = this;
+				if (loadTitleJob != thisJob)
+					return Status.CANCEL_STATUS;
+
+				System.err.println("**********");
+				Object entity = retrieveEntityForEditorTitle(monitor);
+				if (entity != null) {
+					addChangeListener(entity);
+					final String title = getEditorTitleFromEntity(entity);
+					if (title != null) {
+						Display.getDefault().asyncExec(new Runnable() {
+							public void run() {
+								if (loadTitleJob != thisJob)
+									return;
+
+								entityTitle = title;
+								loadTitleJob = null;
+								setPartName(title);
+							}
+						});
+					}
+				}
+
+				return Status.OK_STATUS;
+			}
+		};
+		job.setRule(schedulingRule);
+		this.loadTitleJob = job;
+		job.schedule();
+	}
+
+	private ISchedulingRule schedulingRule = new ISchedulingRule() {
+		@Override
+		public boolean contains(ISchedulingRule rule) {
+			return rule == this;
+		}
+		@Override
+		public boolean isConflicting(ISchedulingRule rule) {
+			return rule == this;
+		}
+	};
+
+	/**
+	 * Get the title for this editor from the {@link #retrieveEntityForEditorTitle(ProgressMonitor) previously retrieved} entity.
+	 * If this method returns <code>null</code>, the editor-title is not changed.
+	 * <p>
+	 * The default implementation of this method returns <code>null</code>.
+	 * </p>
+	 * <p>
+	 * Override this method, as well as {@link #retrieveEntityForEditorTitle(ProgressMonitor)}, to
+	 * determine a nice editor-title.
+	 * </p>
+	 *
+	 * @param entity the object that was previously returned by {@link #retrieveEntityForEditorTitle(ProgressMonitor)}; never <code>null</code>.
+	 * @return <code>null</code> or the title for this editor.
+	 * @see #retrieveEntityForEditorTitle(ProgressMonitor)
+	 */
+	protected String getEditorTitleFromEntity(Object entity)
+	{
+		return null;
+	}
+
+	/**
+	 * Obtain the data that is needed for displaying the correct editor title.
+	 * This is usually the entity for which this editor is opened, detached with minimal
+	 * fetch-groups (only those necessary for determining the title).
+	 * <p>
+	 * If this method returns <code>null</code>, no further action is done by the framework.
+	 * Otherwise, if the returned object is a JDO-object, the framework registers a listener
+	 * on it and re-triggers this method whenever the object changed. Furthermore, the method
+	 * {@link #getEditorTitleFromEntity(Object)} is called for every non-<code>null</code> result.
+	 * </p>
+	 * <p>
+	 * The default implementation of this method returns <code>null</code>.
+	 * </p>
+	 * <p>
+	 * Override this method, as well as {@link #getEditorTitleFromEntity(Object)}, to
+	 * determine a nice editor-title.
+	 * </p>
+	 *
+	 * @param monitor a {@link ProgressMonitor} for progress feedback.
+	 * @return <code>null</code> or the entity from which the title will be obtained in the
+	 *		{@link #getEditorTitleFromEntity(Object)} method.
+	 * @see #getEditorTitleFromEntity(Object)
+	 */
+	protected Object retrieveEntityForEditorTitle(ProgressMonitor monitor)
+	{
+		monitor.done();
+		return null;
+	}
+
+}
