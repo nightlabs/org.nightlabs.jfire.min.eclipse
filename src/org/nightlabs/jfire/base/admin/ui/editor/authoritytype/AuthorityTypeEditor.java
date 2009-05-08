669258ec15a69da1e2a84cdc797fe71e15fa2dd2
khaireel <khaireel@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-05-08 16:47:48 +0000

    Upgraded the EntityEditor to conform to the ActiveEntityEditor.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.admin.ui@14517 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/admin/ui/editor/authoritytype/AuthorityTypeEditor.java
+++ src/org/nightlabs/jfire/base/admin/ui/editor/authoritytype/AuthorityTypeEditor.java
@@ -2,48 +2,52 @@ package org.nightlabs.jfire.base.admin.ui.editor.authoritytype;
 
 import javax.jdo.FetchPlan;
 
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.swt.widgets.Display;
 import org.nightlabs.base.ui.editor.JDOObjectEditorInput;
-import org.nightlabs.base.ui.entity.editor.EntityEditor;
-import org.nightlabs.base.ui.job.Job;
 import org.nightlabs.jdo.NLJDOHelper;
-import org.nightlabs.jfire.base.admin.ui.resource.Messages;
+import org.nightlabs.jfire.base.ui.entity.editor.ActiveEntityEditor;
 import org.nightlabs.jfire.base.ui.login.part.ICloseOnLogoutEditorPart;
 import org.nightlabs.jfire.security.AuthorityType;
 import org.nightlabs.jfire.security.dao.AuthorityTypeDAO;
 import org.nightlabs.jfire.security.id.AuthorityTypeID;
 import org.nightlabs.progress.ProgressMonitor;
 
-public class AuthorityTypeEditor
-extends EntityEditor
-implements ICloseOnLogoutEditorPart
-{
+public class AuthorityTypeEditor extends ActiveEntityEditor
+implements ICloseOnLogoutEditorPart {
+	@Override
+	protected String getEditorTitleFromEntity(Object entity) {
+		return entity instanceof AuthorityType ? ((AuthorityType)entity).getName().getText() : null;
+	}
 
 	@Override
-	public String getTitle() {
-		if(getEditorInput() == null)
-			return super.getTitle();
-
-		Job loadTitleJob = new Job(Messages.getString("org.nightlabs.jfire.base.admin.ui.editor.authoritytype.AuthorityTypeEditor.job.loadingAuthorityType")) { //$NON-NLS-1$
-			@Override
-			protected IStatus run(ProgressMonitor monitor) throws Exception {
-				final String title = AuthorityTypeDAO.sharedInstance().getAuthorityType(
-						(AuthorityTypeID) ((JDOObjectEditorInput<?>)getEditorInput()).getJDOObjectID(),
-						new String[] { FetchPlan.DEFAULT, AuthorityType.FETCH_GROUP_NAME },
-						NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, monitor).getName().getText();
-				Display.getDefault().asyncExec(new Runnable() {
-					public void run() {
-						setPartName(title);
-					}
-				});
-				return Status.OK_STATUS;
-			}
-		};
-		loadTitleJob.schedule();
-
-		return super.getTitle();
+	protected Object retrieveEntityForEditorTitle(ProgressMonitor monitor) {
+		AuthorityTypeID authorityTypeID = (AuthorityTypeID) ((JDOObjectEditorInput<?>)getEditorInput()).getJDOObjectID();
+		assert authorityTypeID != null;
+		return AuthorityTypeDAO.sharedInstance().getAuthorityType(authorityTypeID, new String[] { FetchPlan.DEFAULT, AuthorityType.FETCH_GROUP_NAME }, NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, monitor);
 	}
+
+//	@Override
+//	public String getTitle() {
+//		if(getEditorInput() == null)
+//			return super.getTitle();
+//
+//		Job loadTitleJob = new Job(Messages.getString("org.nightlabs.jfire.base.admin.ui.editor.authoritytype.AuthorityTypeEditor.job.loadingAuthorityType")) { //$NON-NLS-1$
+//			@Override
+//			protected IStatus run(ProgressMonitor monitor) throws Exception {
+//				final String title = AuthorityTypeDAO.sharedInstance().getAuthorityType(
+//						(AuthorityTypeID) ((JDOObjectEditorInput<?>)getEditorInput()).getJDOObjectID(),
+//						new String[] { FetchPlan.DEFAULT, AuthorityType.FETCH_GROUP_NAME },
+//						NLJDOHelper.MAX_FETCH_DEPTH_NO_LIMIT, monitor).getName().getText();
+//				Display.getDefault().asyncExec(new Runnable() {
+//					public void run() {
+//						setPartName(title);
+//					}
+//				});
+//				return Status.OK_STATUS;
+//			}
+//		};
+//		loadTitleJob.schedule();
+//
+//		return super.getTitle();
+//	}
 
 }
