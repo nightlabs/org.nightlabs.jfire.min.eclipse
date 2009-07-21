7da7974e6317dd0eb9cc2bff89d63d0820507a0f
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-07-21 12:39:37 +0000

    - fixed issues 0001195-0001199
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.admin.ui@15090 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/admin/ui/user/CreateUserPage.java
+++ src/org/nightlabs/jfire/base/admin/ui/user/CreateUserPage.java
@@ -38,12 +38,14 @@ import org.nightlabs.base.ui.composite.FormularChangeListener;
 import org.nightlabs.base.ui.composite.FormularChangedEvent;
 import org.nightlabs.base.ui.resource.SharedImages;
 import org.nightlabs.base.ui.wizard.DynamicPathWizardPage;
+import org.nightlabs.jdo.ObjectIDUtil;
 import org.nightlabs.jfire.base.JFireEjb3Factory;
 import org.nightlabs.jfire.base.admin.ui.BaseAdminPlugin;
 import org.nightlabs.jfire.base.admin.ui.resource.Messages;
 import org.nightlabs.jfire.base.ui.login.Login;
 import org.nightlabs.jfire.security.JFireSecurityManagerRemote;
 import org.nightlabs.jfire.security.User;
+import org.nightlabs.jfire.security.UserLocal;
 import org.nightlabs.jfire.security.id.UserID;
 
 /**
@@ -53,85 +55,102 @@ import org.nightlabs.jfire.security.id.UserID;
  */
 public class CreateUserPage extends DynamicPathWizardPage implements FormularChangeListener
 {
-  private Text userID;
-  private Text password0;
-  private Text password1;
-  private Text description;
-  private Text name;
-  private Button autogenerateNameCheckbox;
+	private Text userID;
+	private Text password0;
+	private Text password1;
+	private Text description;
+	private Text name;
+	private Button autogenerateNameCheckbox;
 
-  public CreateUserPage()
-  {
-    super(
-    		CreateUserPage.class.getName(),
-    		Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.title"),  //$NON-NLS-1$
-    		SharedImages.getWizardPageImageDescriptor(BaseAdminPlugin.getDefault(), CreateUserPage.class)
-    	);
-    setDescription(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.description")); //$NON-NLS-1$
-  }
+	public CreateUserPage()
+	{
+		super(
+				CreateUserPage.class.getName(),
+				Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.title"),  //$NON-NLS-1$
+				SharedImages.getWizardPageImageDescriptor(BaseAdminPlugin.getDefault(), CreateUserPage.class)
+		);
+		setDescription(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.description")); //$NON-NLS-1$
+	}
 
-  @Override
-  public Control createPageContents(Composite parent) {
-  	Formular f = new Formular(parent, SWT.NONE, this);
+	@Override
+	public Control createPageContents(Composite parent) {
+		Formular f = new Formular(parent, SWT.NONE, this);
 
-  	userID = f.addTextInput(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.userID.labelText"), null); //$NON-NLS-1$
-  	autogenerateNameCheckbox = f.addCheckBox(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.checkbox.autogenerateName"), false); //$NON-NLS-1$
-  	name = f.addTextInput(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.name.labelText"), null); //$NON-NLS-1$
-  	description = f.addTextInput(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.description.labelText"), null); //$NON-NLS-1$
-  	password0 = f.addTextInput(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.password.labelText"), null); //$NON-NLS-1$
-  	password0.setEchoChar('*');
-  	password1 = f.addTextInput(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.passwordConfirmation.labelText"), null); //$NON-NLS-1$
-  	password1.setEchoChar('*');
+		userID = f.addTextInput(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.userID.labelText"), null); //$NON-NLS-1$
+		autogenerateNameCheckbox = f.addCheckBox(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.checkbox.autogenerateName"), false); //$NON-NLS-1$
+		name = f.addTextInput(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.name.labelText"), null); //$NON-NLS-1$
+		description = f.addTextInput(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.description.labelText"), null); //$NON-NLS-1$
+		password0 = f.addTextInput(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.password.labelText"), null); //$NON-NLS-1$
+		password0.setEchoChar('*');
+		password1 = f.addTextInput(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.passwordConfirmation.labelText"), null); //$NON-NLS-1$
+		password1.setEchoChar('*');
 
-  	// listeners
-  	autogenerateNameCheckbox.addSelectionListener(new SelectionListener() {
+		// listeners
+		autogenerateNameCheckbox.addSelectionListener(new SelectionListener() {
 			public void widgetDefaultSelected(SelectionEvent e) {}
 			public void widgetSelected(SelectionEvent e) {
-				name.setEnabled(!autogenerateNameCheckbox.getSelection());
+				autogenerateNameCheckboxToggled();
 			}
-  	});
+		});
+		autogenerateNameCheckbox.setSelection(true);
+		autogenerateNameCheckboxToggled();
 
-  	verifyInput();
-  	setControl(f);
+//		verifyInput();
+		setControl(f);
 
-  	return f;
-  }
+		return f;
+	}
 
-  private void verifyInput()
-  {
-    try
-    {
-      if(getUserID().length() == 0)
-        updateStatus(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.errorUserIDMissing")); //$NON-NLS-1$
-      else if (getUserID().startsWith(User.USER_ID_PREFIX_TYPE_ORGANISATION))
-      	updateStatus(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.errorUserIDBeginsWithIllegalCharacter") + User.USER_ID_PREFIX_TYPE_ORGANISATION); //$NON-NLS-1$
-//      else if (getUserID().startsWith(User.USERID_PREFIX_TYPE_USERGROUP))
-//      	updateStatus(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.errorUserIDBeginsWithIllegalCharacter") + User.USERID_PREFIX_TYPE_USERGROUP); //$NON-NLS-1$
-      else {
-      	// TODO we should ask for a list of all userIDs and cache it here - so we don't need to ask the server again and again
-      	// especially, all expensive work should be done asynchronously - not on the UI thread!
-      	JFireSecurityManagerRemote userManager = JFireEjb3Factory.getRemoteBean(JFireSecurityManagerRemote.class, Login.getLogin().getInitialContextProperties());
-      	if (userManager.userIDAlreadyRegistered(UserID.create(Login.getLogin().getOrganisationID(), getUserID())) == true)
-      		updateStatus(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.errorUserIDCollision")); //$NON-NLS-1$
-      	else if("".equals(getPassword0()) || "".equals(getPassword1())) //$NON-NLS-1$ //$NON-NLS-2$
-      		updateStatus(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.errorPasswordMissing")); //$NON-NLS-1$
-      	else if(!getPassword0().equals(getPassword1()))
-      		updateStatus(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.errorPasswordConfirmationDoesNotMatch")); //$NON-NLS-1$
-      	else
-      		updateStatus(null);
-      }
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
+	private void autogenerateNameCheckboxToggled() {
+		name.setEnabled(!autogenerateNameCheckbox.getSelection());
+	}
+	
+	private boolean pristine = true;
 
-  /**
-   * Get the user name
-   * @return the user name
-   */
-  public String getUserName() {
-  	return name.getText();
-  }
+	private void verifyInput()
+	{
+		try
+		{
+			if(getUserID().length() == 0)
+				updateStatus(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.errorUserIDMissing")); //$NON-NLS-1$
+			else if (getUserID().startsWith(User.USER_ID_PREFIX_TYPE_ORGANISATION))
+				updateStatus(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.errorUserIDBeginsWithIllegalCharacter") + User.USER_ID_PREFIX_TYPE_ORGANISATION); //$NON-NLS-1$
+			else if (!ObjectIDUtil.isValidIDString(getUserID()))
+				updateStatus("The user-ID is not a valid identifier!");
+			//      else if (getUserID().startsWith(User.USERID_PREFIX_TYPE_USERGROUP))
+			//      	updateStatus(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.errorUserIDBeginsWithIllegalCharacter") + User.USERID_PREFIX_TYPE_USERGROUP); //$NON-NLS-1$
+			else {
+				// TODO we should ask for a list of all userIDs and cache it here - so we don't need to ask the server again and again
+				// especially, all expensive work should be done asynchronously - not on the UI thread!
+				JFireSecurityManagerRemote userManager = JFireEjb3Factory.getRemoteBean(JFireSecurityManagerRemote.class, Login.getLogin().getInitialContextProperties());
+				if (userManager.userIDAlreadyRegistered(UserID.create(Login.getLogin().getOrganisationID(), getUserID())) == true)
+					updateStatus(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.errorUserIDCollision")); //$NON-NLS-1$
+				else if("".equals(getPassword0())) //$NON-NLS-1$ //$NON-NLS-2$
+					updateStatus(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.errorPasswordMissing")); //$NON-NLS-1$
+				else if (!UserLocal.isValidPassword(getPassword0()))
+					updateStatus("The password is not valid (probably it's too short).");
+				else if(!getPassword0().equals(getPassword1()))
+					updateStatus(Messages.getString("org.nightlabs.jfire.base.admin.ui.user.CreateUserPage.errorPasswordConfirmationDoesNotMatch")); //$NON-NLS-1$
+				else
+					updateStatus(null);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Override
+	public boolean isPageComplete() {
+		return !pristine && super.isPageComplete();
+	}
+
+	/**
+	 * Get the user name
+	 * @return the user name
+	 */
+	public String getUserName() {
+		return name.getText();
+	}
 
 	/**
 	 * Get the user description.
@@ -182,6 +201,7 @@ public class CreateUserPage extends DynamicPathWizardPage implements FormularCha
 	 */
 	public void formularChanged(FormularChangedEvent event)
 	{
+		pristine = false;
 		verifyInput();
 	}
 
