230d67db3e125f2265394cee06f312fe66bc3631
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-08-22 09:36:37 +0000

    * The j2ee plugin's runtime copy was never updated before. Now, we check for changes and update if necessary. * Moved the j2ee plugin's runtime copy from the temp directory into the runtime workspace.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.j2ee@15453 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/j2ee/JFireJ2EEPlugin.java
+++ src/org/nightlabs/jfire/base/j2ee/JFireJ2EEPlugin.java
@@ -27,7 +27,6 @@ package org.nightlabs.jfire.base.j2ee;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -36,19 +35,24 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.StreamTokenizer;
+import java.io.UnsupportedEncodingException;
 import java.io.Writer;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Properties;
 import java.util.Set;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.update.configurator.ConfiguratorUtils;
 import org.nightlabs.jfire.classloader.remote.JFireRCDLDelegate;
 import org.nightlabs.util.IOUtil;
 import org.osgi.framework.Bundle;
@@ -66,6 +70,7 @@ implements BundleActivator
 {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.nightlabs.jfire.base.j2ee";
+	private static final Logger logger = Logger.getLogger(JFireJ2EEPlugin.class);
 
 	// The shared instance
 	private static JFireJ2EEPlugin plugin;
@@ -73,14 +78,14 @@ implements BundleActivator
 	/**
 	 * The constructor
 	 */
-	public JFireJ2EEPlugin() 
+	public JFireJ2EEPlugin()
 	{
 		plugin = this;
 	}
 
 	private Bundle bundle;
 
-	public Bundle getBundle() 
+	public Bundle getBundle()
 	{
 		return bundle;
 	}
@@ -89,7 +94,7 @@ implements BundleActivator
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	@Override
-	public void start(BundleContext context) throws Exception 
+	public void start(BundleContext context) throws Exception
 	{
 		this.bundle = context.getBundle();
 	}
@@ -98,63 +103,74 @@ implements BundleActivator
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	@Override
-	public void stop(BundleContext context) throws Exception 
+	public void stop(BundleContext context) throws Exception
 	{
 		plugin = null;
 	}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-// BEGIN: Must be exactly the same as in org.nightlabs.jfire.base.j2ee.osgi.RemoteClassLoadingHook
-	private String minusHexEncode(String plain) {
-		ByteArrayOutputStream bout = new ByteArrayOutputStream();
-		Writer w = new OutputStreamWriter(bout, Charset.forName("UTF-8"));
+//BEGIN: Must be exactly the same as in org.nightlabs.jfire.base.j2ee.osgi.RemoteClassLoadingHook
+	private String hash(String s)
+	{
+		MessageDigest md;
 		try {
-			w.write(plain);
-			w.close();
-		} catch (IOException e) {
-			// writing into an in-memory-stream should never fail
+			md = MessageDigest.getInstance("SHA-256"); // SHA-256 causes a 64 char hash string (SHA-512 would be 128 chars and maybe too long for windows - can't test)
+		} catch (NoSuchAlgorithmException x) {
+			throw new RuntimeException(x);
+		}
+		try {
+			md.update(s.getBytes("UTF-8"));
+		} catch (UnsupportedEncodingException e) {
 			throw new RuntimeException(e);
 		}
+		byte[] buf = md.digest();
 
-		StringBuffer sb = new StringBuffer();
-		for (byte bb : bout.toByteArray()) {
-			int i = 0xff & bb;
-			if (minusHexLiteralAllowed(i))
-				sb.append((char)i); // this *MUST* be casted to char, because the sb.append(char) method must be called - not sb.append(int) - the result is quite different!
-			else {
-				sb.append('-');
-				String s = Integer.toHexString(i);
-				if (s.length() == 1) {
-					sb.append('0');
-					sb.append(s);
-				}
-				else if (s.length() == 2) {
-					sb.append(s);
-				}
-			}
-		}
-		return sb.toString();
-	}
-
-	private boolean minusHexLiteralAllowed(int c)
-	{
-		return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+		int len = buf.length;
+		int pos = 0;
+		StringBuilder hex = new StringBuilder();
+		while (len-- > 0) {
+				byte ch = buf[pos++];
+				int d = (ch >> 4) & 0xf;
+				hex.append((char)(d >= 10 ? 'a' - 10 + d : '0' + d));
+				d = ch & 0xf;
+				hex.append((char)(d >= 10 ? 'a' - 10 + d : '0' + d));
+		 }
+		 return hex.toString();
 	}
 
 	private File getJ2eePluginRuntimeDir()
 	{
-		File tempDir = new File(System.getProperty("java.io.tmpdir"));
+		//		for (Map.Entry<Object, Object> me : System.getProperties().entrySet())
+		//			System.out.println(me.getKey() + " = " + me.getValue());
+		//
+		// relevant properties:
+		//		osgi.instance.area = file:/home/marco/workspaces/runtime-org.nightlabs.jfire.base.ui.product/
+		//		osgi.syspath = /home/marco/workspaces/jfire.branch-1.0/target-rcp-jfire-max-linux-gtk-x86_64/target/plugins
+		//		osgi.instance.area.default = file:/home/marco/.jfire/workspace/
+		//		osgi.install.area = file:/home/marco/workspaces/jfire.branch-1.0/target-rcp-jfire-max-linux-gtk-x86_64/target/
+		String workspace = System.getProperty("osgi.instance.area");
+		if (workspace == null || workspace.isEmpty())
+			workspace = System.getProperty("osgi.instance.area.default");
 
-		String userName = System.getProperty("user.name");
-		String encodedUserName = minusHexEncode(userName);
+		if (workspace == null || workspace.isEmpty())
+			throw new IllegalStateException("There is neither the system property \"osgi.instance.area\" nor \"osgi.instance.area.default\" set! Are we running in OSGI?");
 
-		File j2eePluginRuntimeDir = new File(new File(tempDir, "jfire." + encodedUserName), "org.nightlabs.jfire.base.j2ee");
-		j2eePluginRuntimeDir = j2eePluginRuntimeDir.getAbsoluteFile();
-		return j2eePluginRuntimeDir;
+		String osgiInstallArea = System.getProperty("osgi.install.area");
+
+		if (workspace.startsWith("file:")) {
+			workspace = workspace.substring("file:".length());
+			File workspaceDir = new File(workspace);
+
+			File j2eePluginRuntimeDir = new File(new File(workspaceDir, "data"), "rcl");
+			j2eePluginRuntimeDir = new File(j2eePluginRuntimeDir, hash(osgiInstallArea));
+			j2eePluginRuntimeDir = new File(j2eePluginRuntimeDir, "org.nightlabs.jfire.base.j2ee");
+			j2eePluginRuntimeDir = j2eePluginRuntimeDir.getAbsoluteFile();
+			return j2eePluginRuntimeDir;
+		}
+		throw new UnsupportedOperationException("The system property \"osgi.instance.area\" or \"osgi.instance.area.default\" is not supported! Must start with \"file:\", but does not: " + workspace);
 	}
 //END: Must be exactly the same as in org.nightlabs.jfire.base.j2ee.osgi.RemoteClassLoadingHook
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 	/**
 	 * Returns the shared instance
@@ -201,16 +217,16 @@ implements BundleActivator
 	}
 
 	/**
-	 * Returns a file for the contents of the specified bundle.  Depending 
-	 * on how the bundle is installed the returned file may be a directory or a jar file 
-	 * containing the bundle content.  
-	 * 
+	 * Returns a file for the contents of the specified bundle.  Depending
+	 * on how the bundle is installed the returned file may be a directory or a jar file
+	 * containing the bundle content.
+	 *
 	 * @param bundle the bundle
 	 * @return a file with the contents of the bundle
 	 * @throws IOException if an error occurs during the resolution
-	 * 
+	 *
 	 * @since org.eclipse.equinox.common 3.4
-	 * 
+	 *
 	 * XXX: taken from Equinox 3.4 class FileLocator without any changes (Marc)
 	 */
 	private static File getBundleFile(Bundle bundle) throws IOException {
@@ -228,10 +244,143 @@ implements BundleActivator
 		}
 		throw new IOException("Unknown protocol"); //$NON-NLS-1$
 	}
-	
-	
+
+	private static boolean sourceFileOrDirModified(Properties properties, File source, Set<String> processedSourcePaths)
+	{
+		String sourcePath = source.getPath();
+
+		if (!processedSourcePaths.add(sourcePath))
+			return false;
+
+		if (!source.exists())
+			return true;
+
+		boolean result = false;
+
+		long timestamp = Long.MIN_VALUE;
+		long fileSize = Long.MIN_VALUE;
+
+		if (source.isDirectory()) {
+			for (File f : source.listFiles()) {
+				if (sourceFileOrDirModified(properties, f, processedSourcePaths))
+					result = true;
+			}
+		}
+		else {
+			String timestampS = properties.getProperty(COPY_PROPERTIES_KEY_PREFIX_FILE + sourcePath + COPY_PROPERTIES_KEY_SUFFIX_TIMESTAMP);
+			if (timestampS != null) {
+				try {
+					timestamp = Long.parseLong(timestampS, 36);
+				} catch (NumberFormatException x) {
+					// ignore
+				}
+			}
+
+			String fileSizeS = properties.getProperty(COPY_PROPERTIES_KEY_PREFIX_FILE + sourcePath + COPY_PROPERTIES_KEY_SUFFIX_FILE_SIZE);
+			if (fileSizeS != null) {
+				try {
+					fileSize = Long.parseLong(fileSizeS, 36);
+				} catch (NumberFormatException x) {
+					// ignore
+				}
+			}
+
+			if (source.lastModified() != timestamp || source.length() != fileSize) {
+				result = true;
+				timestamp = source.lastModified();
+				fileSize = source.length();
+				timestampS = Long.toString(timestamp, 36);
+				fileSizeS = Long.toString(fileSize, 36);
+				properties.setProperty(COPY_PROPERTIES_KEY_PREFIX_FILE + sourcePath + COPY_PROPERTIES_KEY_SUFFIX_TIMESTAMP, timestampS);
+				properties.setProperty(COPY_PROPERTIES_KEY_PREFIX_FILE + sourcePath + COPY_PROPERTIES_KEY_SUFFIX_FILE_SIZE, fileSizeS);
+			}
+		}
+
+		return result;
+	}
+
+	private static final String COPY_PROPERTIES_KEY_PREFIX_FILE = "file:";
+	private static final String COPY_PROPERTIES_KEY_SUFFIX_TIMESTAMP = "//timestamp";
+	private static final String COPY_PROPERTIES_KEY_SUFFIX_FILE_SIZE = "//size";
+
+	private static File getSourceDirMetaFile(File destDir)
+	{
+		return new File(destDir, ".source-dir.properties");
+	}
+
+	private static Properties readSourceDirProperties(File destDir)
+	throws IOException
+	{
+		File metaFile = getSourceDirMetaFile(destDir);
+		if (!metaFile.exists())
+			return null;
+
+		Properties properties = new Properties();
+		InputStream in = new FileInputStream(metaFile);
+		try {
+			properties.load(in);
+		} finally {
+			in.close();
+		}
+		return properties;
+	}
+
+	private static boolean sourceFileOrDirModified(Properties properties, Collection<File> sourceDirs)
+	{
+		boolean result = false;
+
+		Set<String> processedSourcePaths = new HashSet<String>();
+
+		// Check, if any of the existing source files/directories is new or has been modified.
+		for (File sourceDir : sourceDirs) {
+			if (sourceFileOrDirModified(properties, sourceDir, processedSourcePaths))
+				result = true;
+		}
+
+		// Check, if any of the previously existing source files/directories does not exist anymore.
+		Set<String> removedSourceFilePaths = new HashSet<String>();
+		for (Object _propKey : properties.keySet()) {
+			String propKey = _propKey.toString();
+			if (!propKey.endsWith(COPY_PROPERTIES_KEY_SUFFIX_TIMESTAMP))
+				continue;
+
+			if (!propKey.startsWith(COPY_PROPERTIES_KEY_PREFIX_FILE))
+				continue;
+
+			String filePath = propKey.substring(
+					COPY_PROPERTIES_KEY_PREFIX_FILE.length(),
+					propKey.length() - COPY_PROPERTIES_KEY_SUFFIX_TIMESTAMP.length()
+			);
+			if (!processedSourcePaths.contains(filePath))
+				removedSourceFilePaths.add(filePath);
+		}
+
+		if (!removedSourceFilePaths.isEmpty()) {
+			for (String sourcePath : removedSourceFilePaths) {
+				properties.remove(COPY_PROPERTIES_KEY_PREFIX_FILE + sourcePath + COPY_PROPERTIES_KEY_SUFFIX_TIMESTAMP);
+				properties.remove(COPY_PROPERTIES_KEY_PREFIX_FILE + sourcePath + COPY_PROPERTIES_KEY_SUFFIX_FILE_SIZE);
+			}
+
+			result = true;
+		}
+
+		return result;
+	}
+
+	private static void writeSourceDirProperties(Properties properties, File destDir)
+	throws IOException
+	{
+		File metaFile = getSourceDirMetaFile(destDir);
+		OutputStream out = new FileOutputStream(metaFile);
+		try {
+			properties.store(out, null);
+		} finally {
+			out.close();
+		}
+	}
+
 	/**
-	 * Copy the bundle if it is deployed as directory or extract it if it is deployed as a jar. 
+	 * Copy the bundle if it is deployed as directory or extract it if it is deployed as a jar.
 	 * @param bundle The bundle
 	 * @param j2eePluginRuntimeDir The target directory
 	 * @throws IOException In case of an error
@@ -248,6 +397,35 @@ implements BundleActivator
 		} else {
 			throw new IOException("Invalid file type: "+bundleFile.getAbsolutePath());
 		}
+	}
+
+	private boolean isJ2eePlugin(URL pluginLocation)
+	{
+		// TODO read the MANIFEST.MF instead of just using the file name / directory name.
+		// Note, that if we find too many plugins here, it doesn't harm. All plugins matching
+		// this method are surveilled for changes. Thus, if we find too many, the system
+		// will react on changes of plugins which don't matter - that's no essential error,
+		// but just a minor in-efficiency. Marco.
+		return pluginLocation.toExternalForm().contains("org.nightlabs.jfire.base.j2ee");
+	}
+
+	private Collection<File> getJ2eePluginLocations()
+	{
+		Set<File> result = new HashSet<File>();
+		URL[] pluginPath = ConfiguratorUtils.getCurrentPlatformConfiguration().getPluginPath();
+		for (URL pluginURL : pluginPath) {
+			if (!isJ2eePlugin(pluginURL))
+				continue;
+
+			if (!"file".equals(pluginURL.getProtocol())) {
+				logger.warn("getJ2eePluginLocations: pluginURL is not a file!!! " + pluginURL);
+				continue;
+			}
+
+			File pluginFile = new File(pluginURL.getPath());
+			result.add(pluginFile);
+		}
+		return result;
 	}
 
 	/**
@@ -286,36 +464,70 @@ implements BundleActivator
 //
 //			manifestFile = new File(realURL.getPath());
 
+			Properties sourceDirProps = null;
 			File j2eePluginRuntimeDir = getJ2eePluginRuntimeDir();
-			if (!j2eePluginRuntimeDir.isDirectory()) {
+			if (j2eePluginRuntimeDir.exists()) {
+				// Is it outdated? If yes, we have to delete it and return true without copying
+				// (because we don't know what to copy [there might be multiple versions installed
+				// in multiple locations and we don't know the search-strategy of the eclipse-plugin-finder]).
+
+				sourceDirProps = readSourceDirProperties(j2eePluginRuntimeDir);
+				if (sourceFileOrDirModified(sourceDirProps, getJ2eePluginLocations())) {
+					IOUtil.deleteDirectoryRecursively(j2eePluginRuntimeDir);
+					return true;
+				}
+			}
+
+			if (!j2eePluginRuntimeDir.exists()) {
 				boolean successful = false;
 				try {
 					j2eePluginRuntimeDir.mkdirs();
 					if (!j2eePluginRuntimeDir.isDirectory())
 						throw new IllegalStateException("Creation of directory failed: " + j2eePluginRuntimeDir);
 
+					changed = true;
 					copyJ2eePluginToRuntimeDir(bundle, j2eePluginRuntimeDir);
 
 					successful = true;
 				} finally {
-					if (!successful) // clean up in case it was only partially created.
+					if (!successful) { // clean up in case it was only partially created.
+						changed = true; // make sure we definitely restart - no matter what
 						IOUtil.deleteDirectoryRecursively(j2eePluginRuntimeDir);
+					}
 				}
-				changed = true;
 
-				manifestFile = new File(j2eePluginRuntimeDir, "META-INF/MANIFEST.MF");
+				if (sourceDirProps == null) {
+					sourceDirProps = new Properties();
+					Collection<File> pluginLocations = getJ2eePluginLocations();
+					File bundleFile = getBundleFile(bundle);
+					if (!pluginLocations.contains(bundleFile)) {
+						logger.error("The current bundle is not contained in the plugin-locations!");
+						logger.error("  * current-bundle: " + bundleFile.getAbsolutePath());
+						for (File pluginLocation : pluginLocations) {
+							logger.error("  * plugin-location: " + pluginLocation.getAbsolutePath());
+						}
+
+						throw new IllegalStateException("The current bundle is not contained in the plugin-locations! " + bundleFile.getAbsolutePath());
+					}
+
+					sourceFileOrDirModified(sourceDirProps, pluginLocations); // just to index the files
+				}
+
+				writeSourceDirProperties(sourceDirProps, j2eePluginRuntimeDir);
 			}
-			else {
-				String bundleLocation = bundle.getLocation();
-				if (!bundleLocation.startsWith("file:"))
-					throw new IllegalStateException("The plugin org.nightlabs.jfire.j2ee is not loaded from its runtime-location, even though that location exists! Its loaded location is " + bundleLocation);
 
-				URL realURL = new URL(bundleLocation);
-				manifestFile = new File(realURL.getPath(), "META-INF/MANIFEST.MF");
-			}
-
-//			if (!realURL.getProtocol().equalsIgnoreCase("file"))
-//				throw new IllegalStateException("The plugin org.nightlabs.jfire.j2ee is not deployed as directory-plugin. Its URL protocol is "+realURL.getProtocol());
+			manifestFile = new File(j2eePluginRuntimeDir, "META-INF/MANIFEST.MF");
+//			if (!j2eePluginRuntimeDirAlreadyExisted) {
+//				manifestFile = new File(j2eePluginRuntimeDir, "META-INF/MANIFEST.MF");
+//			}
+//			else {
+//				String bundleLocation = bundle.getLocation();
+//				if (!bundleLocation.startsWith("file:"))
+//					throw new IllegalStateException("The plugin org.nightlabs.jfire.j2ee is not loaded from its runtime-location, even though that location exists! Its loaded location is " + bundleLocation);
+//
+//				URL realURL = new URL(bundleLocation);
+//				manifestFile = new File(realURL.getPath(), "META-INF/MANIFEST.MF");
+//			}
 
 			if (!manifestFile.exists())
 				throw new IllegalStateException("The plugin's MANIFEST.MF does not exist: " + manifestFile.getAbsolutePath());
