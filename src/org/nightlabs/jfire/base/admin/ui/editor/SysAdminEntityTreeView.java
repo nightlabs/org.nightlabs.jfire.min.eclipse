c0715f750f136ca01eaf341d049b276a48d3c9d9
msaelao <msaelao@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-10-12 10:49:25 +0000

    * Fixed selection bug (correct method was overridden). * Implemented action to grant all access rights to the selected users (in the entity tree).
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.admin.ui@15660 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/admin/ui/editor/SysAdminEntityTreeView.java
+++ src/org/nightlabs/jfire/base/admin/ui/editor/SysAdminEntityTreeView.java
@@ -1,10 +1,10 @@
 /**
- * 
+ *
  */
 package org.nightlabs.jfire.base.admin.ui.editor;
 
-import org.eclipse.swt.widgets.Composite;
 import org.nightlabs.base.ui.entity.tree.EntityTreeView;
+import org.nightlabs.base.ui.part.PartController;
 import org.nightlabs.jfire.base.ui.login.Login;
 import org.nightlabs.jfire.base.ui.login.part.LSDPartController;
 
@@ -16,18 +16,23 @@ public class SysAdminEntityTreeView
 extends EntityTreeView
 {
 	public static final String ID_VIEW = SysAdminEntityTreeView.class.getName();
-	
+
 	public SysAdminEntityTreeView() {
-		LSDPartController.sharedInstance().registerPart(this);
+//		LSDPartController.sharedInstance().registerPart(this);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
-	 */
 	@Override
-	public void createPartControl(Composite parent) {
-		LSDPartController.sharedInstance().createPartControl(this, parent);
+	protected PartController getPartController() {
+		return LSDPartController.sharedInstance();
 	}
+
+//	/* (non-Javadoc)
+//	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
+//	 */
+//	@Override
+//	public void createPartControl(Composite parent) {
+//		LSDPartController.sharedInstance().createPartControl(this, parent);
+//	}
 
 	/* (non-Javadoc)
 	 * @see org.nightlabs.base.ui.part.ControllablePart#canDisplayPart()
