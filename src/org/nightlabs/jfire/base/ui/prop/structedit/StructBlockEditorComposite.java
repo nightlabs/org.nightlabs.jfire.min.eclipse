dc88c2033419a116bf205078c9b1ec65b3d200e4
chairatk <chairatk@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-10-27 15:23:51 +0000

    fixed 0001339: Property set structure editor: Unique flag for struct block cannot be saved  (https://www.jfire.org/modules/bugs/view.php?id=1339)
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.ui@15796 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/ui/prop/structedit/StructBlockEditorComposite.java
+++ src/org/nightlabs/jfire/base/ui/prop/structedit/StructBlockEditorComposite.java
@@ -43,7 +43,7 @@ import org.nightlabs.jfire.prop.validation.ValidationResultType;
 
 public class StructBlockEditorComposite extends XComposite
 {
-	class AddScriptValidatorAction extends SelectionAction 
+	class AddScriptValidatorAction extends SelectionAction
 	{
 		public AddScriptValidatorAction() {
 			super();
@@ -52,12 +52,12 @@ public class StructBlockEditorComposite extends XComposite
 			setId(AddScriptValidatorAction.class.getName());
 			setImageDescriptor(SharedImages.ADD_16x16);
 		}
-		
+
 		@Override
 		public void run() {
 			ScriptDataBlockValidator newValidator = new ScriptDataBlockValidator(
 					ScriptDataBlockValidator.SCRIPT_ENGINE_NAME, "", block); //$NON-NLS-1$
-			ScriptValidatorDialog dialog = new ScriptValidatorDialog(getShell(), null, 
+			ScriptValidatorDialog dialog = new ScriptValidatorDialog(getShell(), null,
 					newValidator, new StructBlockScriptValidatorHandler(block));
 			int returnCode = dialog.open();
 			if (returnCode == Window.OK) {
@@ -72,15 +72,15 @@ public class StructBlockEditorComposite extends XComposite
 				markDirty();
 			}
 		}
-		
+
 		/* (non-Javadoc)
 		 * @see org.nightlabs.base.ui.action.IUpdateActionOrContributionItem#calculateEnabled()
 		 */
 		@Override
-		public boolean calculateEnabled() 
+		public boolean calculateEnabled()
 		{
 			if (block != null) {
-				return !block.getStructFields().isEmpty();	
+				return !block.getStructFields().isEmpty();
 			}
 			return false;
 		}
@@ -91,10 +91,10 @@ public class StructBlockEditorComposite extends XComposite
 		@Override
 		public boolean calculateVisible() {
 			return true;
-		}		
+		}
 	}
-	
-	class AddExpressionValidatorAction extends SelectionAction 
+
+	class AddExpressionValidatorAction extends SelectionAction
 	{
 		public AddExpressionValidatorAction() {
 			super();
@@ -103,10 +103,10 @@ public class StructBlockEditorComposite extends XComposite
 			setId(AddExpressionValidatorAction.class.getName());
 			setImageDescriptor(SharedImages.ADD_16x16);
 		}
-		
+
 		@Override
 		public void run() {
-			ExpressionValidatorDialog dialog = new ExpressionValidatorDialog(getShell(), null, null, 
+			ExpressionValidatorDialog dialog = new ExpressionValidatorDialog(getShell(), null, null,
 					block.getStruct(), new StructBlockExpressionValidatorHandler(block), Mode.STRUCT_BLOCK) ;
 			int returnCode = dialog.open();
 			if (returnCode == Window.OK) {
@@ -126,10 +126,10 @@ public class StructBlockEditorComposite extends XComposite
 		 * @see org.nightlabs.base.ui.action.IUpdateActionOrContributionItem#calculateEnabled()
 		 */
 		@Override
-		public boolean calculateEnabled() 
+		public boolean calculateEnabled()
 		{
 			if (block != null) {
-				return !block.getStructFields().isEmpty();	
+				return !block.getStructFields().isEmpty();
 			}
 			return false;
 		}
@@ -142,8 +142,8 @@ public class StructBlockEditorComposite extends XComposite
 			return true;
 		}
 	}
-	
-	class DeleteValidatorAction extends SelectionAction 
+
+	class DeleteValidatorAction extends SelectionAction
 	{
 		public DeleteValidatorAction() {
 			super();
@@ -152,7 +152,7 @@ public class StructBlockEditorComposite extends XComposite
 			setId(DeleteValidatorAction.class.getName());
 			setImageDescriptor(SharedImages.DELETE_16x16);
 		}
-		
+
 		/* (non-Javadoc)
 		 * @see org.nightlabs.base.ui.action.IUpdateActionOrContributionItem#calculateEnabled()
 		 */
@@ -177,8 +177,8 @@ public class StructBlockEditorComposite extends XComposite
 			markDirty();
 		}
 	}
-	
-	class EditValidatorAction extends SelectionAction 
+
+	class EditValidatorAction extends SelectionAction
 	{
 		public EditValidatorAction() {
 			super();
@@ -187,7 +187,7 @@ public class StructBlockEditorComposite extends XComposite
 			setId(EditValidatorAction.class.getName());
 			setImageDescriptor(SharedImages.EDIT_16x16);
 		}
-		
+
 		/* (non-Javadoc)
 		 * @see org.nightlabs.base.ui.action.IUpdateActionOrContributionItem#calculateEnabled()
 		 */
@@ -220,7 +220,7 @@ public class StructBlockEditorComposite extends XComposite
 			}
 			if (validator instanceof IExpressionValidator) {
 				IExpressionValidator expressionValidator = (IExpressionValidator) validator;
-				ExpressionValidatorDialog dialog = new ExpressionValidatorDialog(getShell(), null, expressionValidator.getExpression(), 
+				ExpressionValidatorDialog dialog = new ExpressionValidatorDialog(getShell(), null, expressionValidator.getExpression(),
 						block.getStruct(), new StructBlockExpressionValidatorHandler(block), Mode.STRUCT_BLOCK) ;
 				dialog.setMessage(expressionValidator.getValidationResult().getI18nValidationResultMessage());
 				dialog.setValidationResultType(expressionValidator.getValidationResult().getResultType());
@@ -229,11 +229,11 @@ public class StructBlockEditorComposite extends XComposite
 					IExpression expression = dialog.getExpressionValidatorComposite().getExpression();
 					I18nText message = dialog.getExpressionValidatorComposite().getMessage();
 					ValidationResultType validationResultType = dialog.getExpressionValidatorComposite().getValidationResultType();
-					expressionValidator.getValidationResult().getI18nValidationResultMessage().copyFrom(message);					
+					expressionValidator.getValidationResult().getI18nValidationResultMessage().copyFrom(message);
 					expressionValidator.getValidationResult().setValidationResultType(validationResultType);
 					expressionValidator.setExpression(expression);
 					validatorTable.refresh();
-					markDirty();					
+					markDirty();
 				}
 			}
 		}
@@ -245,28 +245,28 @@ public class StructBlockEditorComposite extends XComposite
 	private DataBlockValidatorTable validatorTable;
 	private ListenerList listeners;
 	private ToolBarSectionPart sectionPart;
-	
+
 	public StructBlockEditorComposite(Composite parent, int style, LanguageChooser languageChooser) {
 		super(parent, style, LayoutMode.TOP_BOTTOM_WRAPPER, LayoutDataMode.GRID_DATA_HORIZONTAL);
 
 		this.setVisible(false);
 
 		blockNameEditor = new I18nTextEditor(this, languageChooser, Messages.getString("org.nightlabs.jfire.base.ui.prop.structedit.StructBlockEditorComposite.blockNameEditor.caption")); //$NON-NLS-1$
-		
+
 		new Label(this, SWT.SEPARATOR | SWT.HORIZONTAL).setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		
+
 		uniqueButton = new Button(this, SWT.CHECK);
 		uniqueButton.setLayoutData((new GridData()));
 		uniqueButton.setText(Messages.getString("org.nightlabs.jfire.base.ui.prop.structedit.StructBlockEditorComposite.unique.text")); //$NON-NLS-1$
-		
-		sectionPart = new ToolBarSectionPart(new FormToolkit(getDisplay()), this, Section.TITLE_BAR, 
+
+		sectionPart = new ToolBarSectionPart(new FormToolkit(getDisplay()), this, Section.TITLE_BAR,
 				Messages.getString("org.nightlabs.jfire.base.ui.prop.structedit.StructBlockEditorComposite.section.validators.title")); //$NON-NLS-1$
 		validatorTable = new DataBlockValidatorTable(sectionPart.getSection(), SWT.NONE, true, AbstractTableComposite.DEFAULT_STYLE_SINGLE);
 		validatorTable.setLayoutData(new GridData(GridData.FILL_BOTH));
 		sectionPart.getSection().setClient(validatorTable);
-		
+
 		listeners = new ListenerList();
-		
+
 		final EditValidatorAction editAction = new EditValidatorAction();
 		sectionPart.registerAction(new AddExpressionValidatorAction(), true);
 		sectionPart.registerAction(new AddScriptValidatorAction(), true);
@@ -274,26 +274,28 @@ public class StructBlockEditorComposite extends XComposite
 		sectionPart.registerAction(editAction, true);
 		sectionPart.setSelectionProvider(validatorTable);
 		sectionPart.updateToolBarManager();
-		
+
 		validatorTable.addDoubleClickListener(new IDoubleClickListener(){
 			@Override
 			public void doubleClick(DoubleClickEvent event) {
 				editAction.run();
 			}
 		});
-		
+
 		uniqueButton.addSelectionListener(new SelectionAdapter(){
+			@Override
 			public void widgetSelected(SelectionEvent e) {
+				block.setUnique(uniqueButton.getSelection());
 				markDirty();
-			}			
+			}
 		});
-		
+
 		blockNameEditor.addModifyListener(new ModifyListener() {
 			@Override
 			public void modifyText(ModifyEvent e) {
 				markDirty();
 			}
-		});		
+		});
 	}
 
 	public void setStructBlock(StructBlock psb) {
@@ -308,11 +310,11 @@ public class StructBlockEditorComposite extends XComposite
 	public I18nTextEditor getBlockNameEditor() {
 		return blockNameEditor;
 	}
-		
+
 	protected void markDirty() {
 		notifyDataChangeListeners();
 	}
-	
+
 	public void addDataChangeListener(IDataChangeListener listener) {
 		listeners.add(listener);
 	}
@@ -320,13 +322,13 @@ public class StructBlockEditorComposite extends XComposite
 	public void removeDataChangeListener(IDataChangeListener listener) {
 		listeners.remove(listener);
 	}
-	
+
 	protected void notifyDataChangeListeners() {
 		for (Object listener : listeners.getListeners()) {
 			((IDataChangeListener)listener).dataChanged();
 		}
 	}
-	
+
 	public void setEnabled(boolean enabled, boolean validatorEnablement) {
 		super.setEnabled(enabled);
 		sectionPart.getSection().setEnabled(validatorEnablement);
