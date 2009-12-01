6ed873b72ae93cddde46b610331ef82c031894ec
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2009-12-01 23:49:49 +0000

    Fixed wrong class in error message.
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.admin.ui@16342 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/admin/ui/editor/workstation/WorkstationFeaturePreferencesTabPage.java
+++ src/org/nightlabs/jfire/base/admin/ui/editor/workstation/WorkstationFeaturePreferencesTabPage.java
@@ -13,7 +13,6 @@ import org.nightlabs.base.ui.entity.editor.EntityEditor;
 import org.nightlabs.base.ui.entity.editor.IEntityEditorPageController;
 import org.nightlabs.base.ui.entity.editor.IEntityEditorPageFactory;
 import org.nightlabs.jfire.base.admin.ui.editor.user.ConfigPreferencesController;
-import org.nightlabs.jfire.base.admin.ui.editor.user.ConfigPreferencesPage;
 import org.nightlabs.jfire.base.admin.ui.editor.user.ConfigPreferencesSection;
 import org.nightlabs.jfire.base.admin.ui.editor.user.IConfigSetupEditor;
 import org.nightlabs.jfire.base.admin.ui.resource.Messages;
@@ -23,9 +22,9 @@ public class WorkstationFeaturePreferencesTabPage
 	extends FormPage
 {
 	public static final String ID_PAGE = WorkstationFeaturePreferencesTabPage.class.getName();
-	
+
 	private ConfigPreferencesSection preferencesSection;
-	
+
 	public static class Factory implements IEntityEditorPageFactory {
 
 		public IFormPage createPage(FormEditor formEditor) {
@@ -36,11 +35,11 @@ public class WorkstationFeaturePreferencesTabPage
 			return new ConfigPreferencesController(editor);
 		}
 	}
-	
+
 	public WorkstationFeaturePreferencesTabPage(FormEditor formEditor) {
 		super(formEditor, ID_PAGE , Messages.getString("org.nightlabs.jfire.base.admin.ui.editor.workstation.WorkstationFeaturePreferencesTabPage.title")); //$NON-NLS-1$
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.forms.editor.FormPage#createFormContent(org.eclipse.ui.forms.IManagedForm)
 	 */
@@ -52,7 +51,7 @@ public class WorkstationFeaturePreferencesTabPage
 		form.setText(Messages.getString("org.nightlabs.jfire.base.admin.ui.editor.workstation.WorkstationFeaturePreferencesTabPage.form.text"));  //$NON-NLS-1$
 		fillBody(managedForm, toolkit);
 	}
-	
+
 	protected void fillBody(IManagedForm form, FormToolkit toolkit) {
 		Composite body = form.getForm().getBody();
 		GridLayout layout = new GridLayout();
@@ -64,12 +63,12 @@ public class WorkstationFeaturePreferencesTabPage
 		layout.horizontalSpacing = 10;
 		body.setLayout(layout);
 		body.setLayoutData(new GridData(GridData.FILL_BOTH));
-	
+
 		if (! (getEditor() instanceof IConfigSetupEditor))
-			throw new IllegalStateException(ConfigPreferencesPage.class.getName() +
+			throw new IllegalStateException(WorkstationFeaturePreferencesTabPage.class.getName() +
 					" should only be used with a Editor implementing IConfigSetupEditor"); //$NON-NLS-1$
 		ConfigID configID = ((IConfigSetupEditor)getEditor()).getConfigID();
-		
+
 		preferencesSection = new ConfigPreferencesSection(this, body, configID);
 		getManagedForm().addPart(preferencesSection);
 	}
