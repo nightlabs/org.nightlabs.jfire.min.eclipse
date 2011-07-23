c4fc5538de4740d0faa2fe34f66495fc1b0abd9d
marco <marco@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2011-07-23 16:23:44 +0000

    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.login.ui@19083 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/base/login/ui/LoginDelegate.java
@@ -0,0 +1,25 @@
+package org.nightlabs.jfire.base.login.ui;
+
+import javax.security.auth.login.LoginException;
+
+import org.nightlabs.base.ui.login.ILoginDelegate;
+import org.nightlabs.base.ui.login.LoginState;
+
+public class LoginDelegate implements ILoginDelegate {
+
+	public LoginState getLoginState()
+	{
+		return Login.sharedInstance().getLoginState();
+	}
+
+	public void login()
+	throws LoginException
+	{
+		Login.getLogin();
+	}
+
+	public void logout() {
+		Login.sharedInstance().logout();
+	}
+
+}
