35bbb4bacdd323a3fbb783db68d146b454690490
alex <alex@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2011-12-12 17:30:07 +0000

    Intial version of dashboard serverside config with skeleton of preference page for dashboard
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.dashboard.ui@19991 18c61ebf-f106-0410-9e1e-81fb82415b8f

---
+++ src/org/nightlabs/jfire/base/dashboard/ui/DashboardGridLayoutConfig.java
@@ -0,0 +1,138 @@
+package org.nightlabs.jfire.base.dashboard.ui;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.nightlabs.clientui.layout.GridData;
+import org.nightlabs.clientui.layout.GridLayout;
+import org.nightlabs.clientui.ui.layout.IGridDataEntry;
+import org.nightlabs.clientui.ui.layout.IGridLayoutConfig;
+import org.nightlabs.jfire.dashboard.DashboardGadgetLayoutEntry;
+import org.nightlabs.jfire.dashboard.DashboardLayoutConfigModule;
+
+/**
+ * 
+ */
+
+/**
+ * @author abieber
+ *
+ */
+public class DashboardGridLayoutConfig<T> implements IGridLayoutConfig {
+
+	private DashboardLayoutConfigModule<T> configModule;
+	
+	private List<IGridDataEntry> entries;
+	private Map<IGridDataEntry, DashboardGadgetLayoutEntry<T>> entryMap;
+	
+	/**
+	 * 
+	 */
+	public DashboardGridLayoutConfig(DashboardLayoutConfigModule<T> configModule) {
+		this.configModule = configModule;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.clientui.ui.layout.IGridLayoutConfig#addGridDataEntry()
+	 */
+	@Override
+	public IGridDataEntry addGridDataEntry() {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.clientui.ui.layout.IGridLayoutConfig#getGridDataEntries()
+	 */
+	@Override
+	public List<IGridDataEntry> getGridDataEntries() {
+		createIndex(false);
+		return entries;
+	}
+
+	private void createIndex(boolean force) {
+		if (entryMap == null || entries == null || force) {
+			entries = new LinkedList<IGridDataEntry>();
+			List<DashboardGadgetLayoutEntry<T>> editLayoutEntries = configModule.getEditLayoutEntries();
+			entryMap = new HashMap<IGridDataEntry, DashboardGadgetLayoutEntry<T>>();
+			for (DashboardGadgetLayoutEntry<T> entry : editLayoutEntries) {
+				IGridDataEntry gridDataEntry = createGridDataEntry(entry);
+				entryMap.put(gridDataEntry, entry);
+				entries.add(gridDataEntry);
+			}
+			
+		}
+	}
+	
+	private IGridDataEntry createGridDataEntry(final DashboardGadgetLayoutEntry<T> layoutEntry) {
+		return new IGridDataEntry() {
+			
+			@Override
+			public String getName() {
+				return layoutEntry.getName();
+			}
+			
+			@Override
+			public GridData getGridData() {
+				return layoutEntry.getGridData();
+			}
+		};
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.clientui.ui.layout.IGridLayoutConfig#getGridLayout()
+	 */
+	@Override
+	public GridLayout getGridLayout() {
+		return configModule.getGridLayout();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.clientui.ui.layout.IGridLayoutConfig#moveEntryDown(org.nightlabs.clientui.ui.layout.IGridDataEntry)
+	 */
+	@Override
+	public boolean moveEntryDown(IGridDataEntry gridDataEntry) {
+		createIndex(false);
+		DashboardGadgetLayoutEntry<T> gadgetLayoutEntry = entryMap.get(gridDataEntry);
+		if (gadgetLayoutEntry != null) {
+			try {
+				return configModule.moveEditLayoutEntryDown(gadgetLayoutEntry);
+			} finally {
+				createIndex(true);
+			}
+		}
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.clientui.ui.layout.IGridLayoutConfig#moveEntryUp(org.nightlabs.clientui.ui.layout.IGridDataEntry)
+	 */
+	@Override
+	public boolean moveEntryUp(IGridDataEntry gridDataEntry) {
+		createIndex(false);
+		DashboardGadgetLayoutEntry<T> gadgetLayoutEntry = entryMap.get(gridDataEntry);
+		if (gadgetLayoutEntry != null) {
+			try {
+				return configModule.moveEditLayoutEntryUp(gadgetLayoutEntry);
+			} finally {
+				createIndex(true);
+			}
+		}
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.clientui.ui.layout.IGridLayoutConfig#removeGridDataEntry(org.nightlabs.clientui.ui.layout.IGridDataEntry)
+	 */
+	@Override
+	public void removeGridDataEntry(IGridDataEntry gridDataEntry) {
+		createIndex(false);
+		DashboardGadgetLayoutEntry<T> gadgetLayoutEntry = entryMap.get(gridDataEntry);
+		if (gadgetLayoutEntry != null) {
+			configModule.removeEditLayoutEntry(gadgetLayoutEntry);
+			createIndex(true);
+		}
+	}
+
+}
---
+++ src/org/nightlabs/jfire/base/dashboard/ui/DashboardGridLayoutPreferencePage.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package org.nightlabs.jfire.base.dashboard.ui;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.nightlabs.clientui.ui.layout.IGridLayoutConfig;
+import org.nightlabs.jfire.base.ui.config.AbstractConfigModuleController;
+import org.nightlabs.jfire.base.ui.config.AbstractConfigModulePreferencePage;
+import org.nightlabs.jfire.base.ui.config.IConfigModuleController;
+import org.nightlabs.jfire.base.ui.layout.AbstractEditLayoutPreferencePage;
+import org.nightlabs.jfire.config.ConfigModule;
+import org.nightlabs.jfire.dashboard.DashboardLayoutConfigModule;
+import org.nightlabs.jfire.layout.AbstractEditLayoutConfigModule;
+import org.nightlabs.jfire.layout.AbstractEditLayoutEntry;
+
+/**
+ * @author abieber
+ *
+ */
+public class DashboardGridLayoutPreferencePage extends
+		AbstractEditLayoutPreferencePage {
+
+	private static class Controller extends AbstractConfigModuleController {
+		
+		private static final Set<String> CONFIG_MODULE_FETCH_GROUPS = new HashSet<String>();
+		
+		public Controller(AbstractConfigModulePreferencePage preferencePage) {
+			super(preferencePage);
+		}
+
+		@Override
+		public Class<? extends ConfigModule> getConfigModuleClass() {
+			return DashboardLayoutConfigModule.class;
+		}
+
+		@Override
+		public Set<String> getConfigModuleFetchGroups() {
+			if (CONFIG_MODULE_FETCH_GROUPS.isEmpty()) {
+				CONFIG_MODULE_FETCH_GROUPS.addAll(super.getCommonConfigModuleFetchGroups());
+				CONFIG_MODULE_FETCH_GROUPS.add(AbstractEditLayoutConfigModule.FETCH_GROUP_GRID_LAYOUT);
+				CONFIG_MODULE_FETCH_GROUPS.add(AbstractEditLayoutConfigModule.FETCH_GROUP_EDIT_LAYOUT_ENTRIES);
+				CONFIG_MODULE_FETCH_GROUPS.add(AbstractEditLayoutEntry.FETCH_GROUP_GRID_DATA);
+			}
+			return CONFIG_MODULE_FETCH_GROUPS;
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	public DashboardGridLayoutPreferencePage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param useTabFolder
+	 */
+	public DashboardGridLayoutPreferencePage(boolean useTabFolder) {
+		super(useTabFolder);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.base.ui.layout.AbstractEditLayoutPreferencePage#createConfigModuleGridLayoutConfig()
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	protected IGridLayoutConfig createConfigModuleGridLayoutConfig() {
+		return new DashboardGridLayoutConfig((DashboardLayoutConfigModule) getConfigModuleController().getConfigModule());
+	}
+
+	@Override
+	public Controller getConfigModuleController() {
+		return (Controller) super.getConfigModuleController();
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.base.ui.layout.AbstractEditLayoutPreferencePage#getUseCaseDescription()
+	 */
+	@Override
+	public String getUseCaseDescription() {
+		return "JFire Dashboard";
+	}
+
+	/* (non-Javadoc)
+	 * @see org.nightlabs.jfire.base.ui.config.AbstractConfigModulePreferencePage#createConfigModuleController()
+	 */
+	@Override
+	protected IConfigModuleController createConfigModuleController() {
+		return new Controller(this);
+	}
+
+}
