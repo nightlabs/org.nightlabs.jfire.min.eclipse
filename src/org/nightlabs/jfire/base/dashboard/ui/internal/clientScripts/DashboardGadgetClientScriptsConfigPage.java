345941c03e2f6a1f2ee1fe7ac102750b4384b8ea
frederik <frederik@18c61ebf-f106-0410-9e1e-81fb82415b8f>
2011-12-28 17:59:38 +0000

    * started to implement config page actions
    git-svn-id: https://svn.jfire.org/svn/main/trunk/org.nightlabs.jfire.base.dashboard.ui@20179 18c61ebf-f106-0410-9e1e-81fb82415b8f

--- src/org/nightlabs/jfire/base/dashboard/ui/internal/clientScripts/DashboardGadgetClientScriptsConfigPage.java
+++ src/org/nightlabs/jfire/base/dashboard/ui/internal/clientScripts/DashboardGadgetClientScriptsConfigPage.java
@@ -4,13 +4,22 @@ import java.util.List;
 
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
 import org.nightlabs.base.ui.composite.XComposite;
 import org.nightlabs.base.ui.composite.XComposite.LayoutMode;
 import org.nightlabs.base.ui.language.I18nTextEditor;
@@ -27,13 +36,21 @@ public class DashboardGadgetClientScriptsConfigPage extends AbstractDashbardGadg
 	
 	private Button buttonConfirmProcessing;
 	
-	private Table table;
-	
 	private List<DashboardGadgetClientScriptsConfig.ClientScript> clientScripts;
 	
 	private boolean confirmProcessing;
 	
 	private TableViewer tableViewer;
+	
+	private Button buttonRemove;
+	
+	private Button buttonEdit;
+	
+	private Button buttonMoveUp;
+	
+	private Button buttonMoveDown;
+	
+	private DashboardGadgetClientScriptsConfig config;
 	
 	public DashboardGadgetClientScriptsConfigPage() {
 		super(DashboardGadgetClientScriptsConfigPage.class.getName());
@@ -42,15 +59,15 @@ public class DashboardGadgetClientScriptsConfigPage extends AbstractDashbardGadg
 
 	@Override
 	public Control createPageContents(final Composite parent) {
-		final XComposite wrapper = new XComposite(parent, SWT.NONE, LayoutMode.TIGHT_WRAPPER, 2);
+		final XComposite wrapper = new XComposite(parent, SWT.NONE, LayoutMode.TIGHT_WRAPPER, 3);
 		
 		final Label labelDescription1 = new Label(wrapper, SWT.WRAP);
-		labelDescription1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		labelDescription1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 3, 1));
 		labelDescription1.setText("This gadget will show you all stored client scripts.");
 		
 		final Label labelTitle = new Label(wrapper, SWT.NONE);
-		labelTitle.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
-		labelTitle.setText("Select the title for this gadget:");
+		labelTitle.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1));
+		labelTitle.setText("Set the title of this gadget:");
 
 		gadgetTitle = new I18nTextEditor(wrapper);
 		gadgetTitle.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
@@ -58,9 +75,9 @@ public class DashboardGadgetClientScriptsConfigPage extends AbstractDashbardGadg
 		
 		createUpperWidgets(wrapper);
 		createTableWidget(wrapper);
-		createTableSpecificWidgets(wrapper);
+		createButtonWidgets(wrapper);
 		
-		setTableInput();
+		tableViewer.setInput(clientScripts);
 		
 		return wrapper;
 	}
@@ -69,23 +86,43 @@ public class DashboardGadgetClientScriptsConfigPage extends AbstractDashbardGadg
 		GridData gd;
 		
 		buttonConfirmProcessing = new Button(parent, SWT.CHECK);
-		gd = new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1);
+		gd = new GridData(SWT.FILL, SWT.CENTER, true, false, 3, 1);
 		gd.verticalIndent = 10;
 		buttonConfirmProcessing.setLayoutData(gd);
 		buttonConfirmProcessing.setText("Confirm processing");
 		buttonConfirmProcessing.setEnabled(confirmProcessing);
 		
 		final Label labelDescription2 = new Label(parent, SWT.WRAP);
-		labelDescription2.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		labelDescription2.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 3, 1));
 		labelDescription2.setText("Select a client script to be edited or create a new one.");
 
 	}
 	
 	private void createTableWidget(final Composite parent) {
-		table = new Table(parent, SWT.BORDER | SWT.FULL_SELECTION);
-		final GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 5);
+		final Table table = new Table(parent, SWT.BORDER | SWT.FULL_SELECTION | SWT.MULTI);
+		final GridData gridData = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1);
 		gridData.verticalIndent = 5;
 		table.setLayoutData(gridData);
+		
+		table.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(final Event event) {
+				updateButtonStates();
+				
+				
+				
+			}
+		});
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDoubleClick(final MouseEvent e) {
+				if (table.getSelectionIndex() > -1) {
+					final TableItem item = table.getItem(table.getSelectionIndex());
+					
+					
+					
+				}
+			}
+		});
 		
 		tableViewer = new TableViewer(table);
 		
@@ -93,39 +130,148 @@ public class DashboardGadgetClientScriptsConfigPage extends AbstractDashbardGadg
 	    attachLabelProvider();
 	}
 	
-	private void createTableSpecificWidgets(final Composite parent) {
+	private void createButtonWidgets(final Composite parent_) {
+		final Composite parent = new Composite(parent_, SWT.NONE);
+		parent.setLayout(new GridLayout());
 		GridData gd;
 		
 		final Button buttonNew = new Button(parent, SWT.PUSH);
-		gd = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd.widthHint = 75;
+		gd = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
 		buttonNew.setLayoutData(gd);
 		buttonNew.setText("New");
+		buttonNew.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+				createClientScript();
+				updateButtonStates();
+			}
+		});
 		
-		final Button buttonRemove = new Button(parent, SWT.PUSH);
-		gd = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd.widthHint = 75;
+		buttonRemove = new Button(parent, SWT.PUSH);
+		gd = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
 		buttonRemove.setLayoutData(gd);
 		buttonRemove.setText("Remove");
+		buttonRemove.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+				removeClientScript();
+				updateButtonStates();
+			}
+		});
 		
-		final Button buttonEdit = new Button(parent, SWT.PUSH);
-		gd = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd.widthHint = 75;
+		buttonEdit = new Button(parent, SWT.PUSH);
+		gd = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
 		buttonEdit.setLayoutData(gd);
 		buttonEdit.setText("Edit");
+		buttonEdit.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+				editClientScript();
+//				updateButtonStates();
+			}
+		});
 
-		final Button buttonUp = new Button(parent, SWT.PUSH);
-		gd = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd.widthHint = 75;
-		buttonUp.setLayoutData(gd);
-		buttonUp.setText("Up");
+		buttonMoveUp = new Button(parent, SWT.PUSH);
+		gd = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		buttonMoveUp.setLayoutData(gd);
+		buttonMoveUp.setEnabled(false);
+		buttonMoveUp.setText("Up");
+		buttonMoveUp.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+				updateItemOrder();
+				updateButtonStates();
+			}
+		});
 
-		final Button buttonDown = new Button(parent, SWT.PUSH);
-		gd = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
-		gd.widthHint = 75;
-		buttonDown.setLayoutData(gd);
-		buttonDown.setText("Down");
+		buttonMoveDown = new Button(parent, SWT.PUSH);
+		gd = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		buttonMoveDown.setLayoutData(gd);
+		buttonMoveDown.setEnabled(false);
+		buttonMoveDown.setText("Down");
+		buttonMoveDown.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+				updateItemOrder();
+				updateButtonStates();
+			}
+		});
+	}
+	
+	static class ClientScriptPropertiesWrapper {
 		
+		private String clientScriptName;
+		private String clientScriptContent;
+		
+		public ClientScriptPropertiesWrapper() {
+			clientScriptName = "";
+			clientScriptContent = "";
+		}
+		
+		public ClientScriptPropertiesWrapper(final String clientScriptName, final String clientScriptContent) {
+			this.clientScriptName = clientScriptName;
+			this.clientScriptContent = clientScriptContent;
+		}
+
+		public String getClientScriptName() {
+			return clientScriptName;
+		}
+		public String getClientScriptContent() {
+			return clientScriptContent;
+		}
+		public void setClientScriptName(final String clientScriptName) {
+			this.clientScriptName = clientScriptName;
+		}
+		public void setClientScriptContent(final String clientScriptContent) {
+			this.clientScriptContent = clientScriptContent;
+		}
+	}
+	
+	private void createClientScript() {
+		final ClientScriptPropertiesWrapper data = new ClientScriptPropertiesWrapper();
+		final DashboardGadgetClientScriptsNewEditDialog dialog = new DashboardGadgetClientScriptsNewEditDialog(getShell(), data);
+		if (dialog.open() == Window.OK) {
+			// Create new ClientScript instance and insert it into the table (not persisted yet)
+			DashboardGadgetClientScriptsConfig.ClientScript newClientScript = config.createNewClientScript(
+				data.getClientScriptName(), data.getClientScriptContent());		// TODO
+//			final DashboardGadgetClientScriptsConfig.ClientScript newClientScript = new DashboardGadgetClientScriptsConfig.ClientScript(
+//				data.getClientScriptName(), data.getClientScriptContent());
+			if (tableViewer.getInput() instanceof List<?>) {
+				final List<DashboardGadgetClientScriptsConfig.ClientScript> clientScripts = (List<DashboardGadgetClientScriptsConfig.ClientScript>) tableViewer.getInput();
+				clientScripts.add(newClientScript);
+				tableViewer.setInput(null);		// TODO
+				tableViewer.setInput(clientScripts);
+			}
+		}
+	}
+	
+	private void editClientScript() {
+		TableItem item = tableViewer.getTable().getItem(tableViewer.getTable().getSelectionIndex());
+		Object data_ = item.getData();
+		if (data_ instanceof DashboardGadgetClientScriptsConfig.ClientScript) {
+			DashboardGadgetClientScriptsConfig.ClientScript clientScript = (DashboardGadgetClientScriptsConfig.ClientScript) data_;
+			ClientScriptPropertiesWrapper data = new ClientScriptPropertiesWrapper(clientScript.getName(), clientScript.getScript());
+			final DashboardGadgetClientScriptsNewEditDialog dialog = new DashboardGadgetClientScriptsNewEditDialog(getShell(), data);
+			if (dialog.open() == Window.OK) {
+				
+			}
+		}
+	}
+	
+	private void removeClientScript() {
+		
+	}
+	
+	private void updateItemOrder() {
+		
+	}
+	
+	private void updateButtonStates() {
+		final boolean validIdx = tableViewer.getTable().getSelectionIndex() > 0 ? true : false;
+		buttonRemove.setEnabled(validIdx);
+		buttonEdit.setEnabled(validIdx);
+		buttonMoveUp.setEnabled(validIdx);
+		buttonMoveDown.setEnabled(tableViewer.getTable().getSelectionIndex() < tableViewer.getTable().getItemCount() - 1);
 	}
 	
 	private void attachContentProvider() {
@@ -144,10 +290,6 @@ public class DashboardGadgetClientScriptsConfigPage extends AbstractDashbardGadg
 		});
 	}
 	
-	private void setTableInput() {
-		tableViewer.setInput(clientScripts);
-	}
-	
 	private I18nText createInitialName() {
 		final I18nTextBuffer textBuffer = new I18nTextBuffer();
 //		TradeDashboardGadgetsConfigModuleInitialiser.initializeClientScriptsGadgetName(textBuffer);
@@ -159,9 +301,8 @@ public class DashboardGadgetClientScriptsConfigPage extends AbstractDashbardGadg
 		super.initialize(layoutEntry);
 		final Object config_ = getLayoutEntry().getConfig();
 		if (config_ instanceof DashboardGadgetClientScriptsConfig) {
-			final DashboardGadgetClientScriptsConfig config = (DashboardGadgetClientScriptsConfig) config_;
+			config = (DashboardGadgetClientScriptsConfig) config_;
 			clientScripts = config.getClientScripts();
-			confirmProcessing = config.isConfirmProcessing();
 		}
 	}
 	
---
+++ src/org/nightlabs/jfire/base/dashboard/ui/internal/clientScripts/DashboardGadgetClientScriptsNewEditDialog.java
@@ -0,0 +1,88 @@
+package org.nightlabs.jfire.base.dashboard.ui.internal.clientScripts;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.nightlabs.base.ui.composite.XComposite;
+import org.nightlabs.base.ui.composite.XComposite.LayoutMode;
+import org.nightlabs.eclipse.ui.dialog.ResizableTitleAreaDialog;
+
+public class DashboardGadgetClientScriptsNewEditDialog extends ResizableTitleAreaDialog {
+
+	DashboardGadgetClientScriptsConfigPage.ClientScriptPropertiesWrapper contentWrapper;
+	
+	public DashboardGadgetClientScriptsNewEditDialog(final Shell shell, final DashboardGadgetClientScriptsConfigPage.ClientScriptPropertiesWrapper 
+		contentWrapper) {
+		
+		super(shell, null);
+		this.contentWrapper = contentWrapper;
+	}
+
+	@Override
+	protected Control createDialogArea(final Composite parent) {
+		final Composite parent_ = (Composite) super.createDialogArea(parent);
+		setTitle("Title");
+		setMessage("Message");
+		
+		final Composite content = new XComposite(parent_, SWT.NONE, LayoutMode.TIGHT_WRAPPER, 2);
+		GridData gd;
+		
+		final Label labelDescription1 = new Label(content, SWT.NONE);
+		gd = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd.verticalIndent = 10;
+		labelDescription1.setLayoutData(gd);
+		labelDescription1.setText("Set the name of the client script:");
+		
+		final Text textClientScriptName = new Text(content, SWT.BORDER);
+		gd = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		gd.verticalIndent = 10;
+		textClientScriptName.setLayoutData(gd);
+		textClientScriptName.setText("Client script name");
+		textClientScriptName.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(final ModifyEvent event) {
+				if (event.getSource() instanceof Text)
+					contentWrapper.setClientScriptName(((Text) event.getSource()).getText());	// TODO add delay
+			}
+		});
+		
+		final Label labelDescription2 = new Label(content, SWT.NONE);
+		gd = new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1);
+		gd.verticalIndent = 10;
+		labelDescription2.setLayoutData(gd);
+		labelDescription2.setText("Set the content of the client script:");
+		
+		final Text textClientScriptContent = new Text(content, SWT.BORDER | SWT.WRAP);
+		gd = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1);
+		textClientScriptContent.setLayoutData(gd);
+		textClientScriptContent.setText("Client script content");
+		textClientScriptContent.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(final ModifyEvent event) {
+				if (event.getSource() instanceof Text)
+					contentWrapper.setClientScriptContent(((Text) event.getSource()).getText());	// TODO add delay
+			}
+		});
+		
+		return parent_;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
